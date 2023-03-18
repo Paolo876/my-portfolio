822f2955fcef8de2885526838b066f5798c2bc0a
paoloBugarin <paolopaolobugarin@gmail.com>
2023-03-18 00:10:00 -0700

    updated ui, implemented materialTheme

--- src/index.js
+++ src/index.js
@@ -2,10 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import { theme } from "./materialTheme";
+import { ThemeProvider } from '@mui/material';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
---
+++ src/materialTheme.js
@@ -0,0 +1,33 @@
+import { createTheme } from '@mui/material';
+
+export const theme = createTheme({
+    palette: {
+        type: 'dark',
+        primary: {
+          main: '#474e8d',
+        },
+        secondary: {
+          main: '#d3547d',
+        },
+        background: {
+          default: '#141E30',
+          paper: '#243B55',
+        },
+        text: {
+          primary: '#FAF9F6',
+        },
+      },
+      typography: {
+        h1: {
+          fontSize: 35,
+          fontFamily: 'Manrope',
+          fontWeight: 600,
+          letterSpacing: '0.01em',
+        },
+        h2: {
+          fontSize: 32,
+          fontFamily: 'Manrope',
+          fontWeight: 600,
+        },
+      },
+});
