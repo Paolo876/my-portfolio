7033eff615fb9dd16620910de08dac1ddf3053e1
paoloBugarin <paolopaolobugarin@gmail.com>
2023-03-22 21:36:05 -0700

    added skills icons on assets

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import PageContainer from '../../components/PageContainer'
 import { Grid, Typography, Box, Button} from '@mui/material'
-import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+
 
 const Home = () => {
   return (
@@ -17,18 +19,21 @@ const Home = () => {
             <Typography>My name is Paolo Bugarin, I am a Full Stack Web Developer based in Los Angeles, California.</Typography>
             <Typography>I design and develop websites from a simple static page to a complex dynamic web application.</Typography>
 
-            <Typography>
-              {/* I am a Computer Information Systems student in Pasadena City College majoring in Web Development and Software Development.  */}
+            {/* <Typography>
               I studied Computer Information Systems majoring in Web Development and Software Development at Pasadena City College.
               A big part of my development knowledge also came from self-studying through varieties of online courses and being a part of coding groups.
-            </Typography>
+            </Typography> */}
           </Box>
           <Box>
-            <Button variant="outlined" sx={{mixBlendMode: "difference", fontWeight: 400}} color="inherit" endIcon={<ArrowForwardIosIcon color="primary"/>}>Read More About Me</Button>
+            <Button variant="outlined" sx={{mixBlendMode: "difference", fontWeight: 400}} color="inherit" endIcon={<KeyboardArrowRightIcon color="primary"/>}>Read More About Me</Button>
           </Box>
           <Box>
             <Typography>I mainly develop my projects using ReactJS or NextJS in the Front End, and NodeJS and ExpressJS on the Backend along with related libraries and frameworks.</Typography>
             <Typography>Here are some of my arsenal of expertise: </Typography>
+
+          </Box>
+          <Box>
+            <Button variant="outlined" sx={{mixBlendMode: "difference", fontWeight: 400}} color="inherit" endIcon={<KeyboardArrowDownIcon color="primary"/>}>See My Works</Button>
           </Box>
         </Grid>
         <Grid item xs={12}>
