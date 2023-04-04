0a9a18ec0489d6dfd00ef43954a24c470c074758
paoloBugarin <paolopaolobugarin@gmail.com>
2023-04-03 20:58:58 -0700

    updated home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,12 @@
-import { useEffect} from 'react'
-import { Box } from '@mui/material'
+import { useEffect } from 'react'
 import Introduction from './Introduction/Introduction'
 import Contact from './Contact/Contact'
 import Skills from './Skills/Skills'
 import Footer from '../../components/Footer'
-
+import DocumentHead from '../../components/DocumentHead'
 import { useAppContext } from '../../hooks/useAppContext'
 import useFirestoreActions from '../../hooks/useFirestoreActions'
+import { Box } from '@mui/material'
 
 const Home = () => {
   const { skillsList, dispatch } = useAppContext();
@@ -21,6 +21,11 @@ const Home = () => {
 
   return (
     <Box sx={{pt: 8, height: "100vh", overflowX: "hidden"}}>
+        <DocumentHead
+          title="Paolo Bugarin | Home"
+          description="I am a full stack web developer based in Los Angeles, California"
+          keyword="react, reactjs, redux, next, nextjs, node, nodejs, mern, express, expressjs, mongodb, sequelize, mysql, developer, fullstack developer, full stack developer, javascript"
+        />
         <Introduction/>
         <Skills/>
         <Contact/>
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -31,48 +31,48 @@ const Landing = () => {
 
   return (
     <PageContainer>
-          <DocumentHead
+      <DocumentHead
         title="Paolo Bugarin | Developer"
         description="I am a full stack web developer based in Los Angeles, California"
         keyword="react, reactjs, redux, next, nextjs, node, nodejs, mern, express, expressjs, mongodb, sequelize, mysql, developer, fullstack developer, full stack developer, javascript"
       />
-        <Grid container sx={{height: "100%", px: 5}}>
-          <Grid item mt={11} xs={12} sm={6}>
-            <Typography variant="h1" sx={nameStyles}>Paolo</Typography>
-            <Typography variant="h1" sx={nameStyles}>Bugarin</Typography>
-            <Typography 
-              variant="h5"
-              sx={{
-                mt:8,
-                fontWeight: 300, 
-                opacity: .75, 
-                mixBlendMode: "difference", 
-                letterSpacing: 2
-              }}
-              
-            >
-              {`< `}FULL STACK DEVELOPER{` />`}
-            </Typography>
-          </Grid>
-
-          <Grid item xs={12} sm={6}>
-            <Stack spacing={1.5} alignItems="flex-end" justifyContent="center" pt={20} sx={{height: "100%"}}>
-              <Box sx={{border: 1, mixBlendMode: "difference"}} my={4}>
-                <IconButton size="large" onClick={() => navigate("/home")} >
-                  <ArrowForwardIosIcon fontSize="medium" color="primary"/>
-                </IconButton>
-              </Box>
-              <Link component={ReactLink} to="/home" draggable={false}><Typography sx={linkStyles}>Home</Typography></Link>
-              <Link component={ReactLink} to="/projects" draggable={false}><Typography sx={linkStyles}>Projects</Typography></Link>
-              <Link component={ReactLink} to="/about" draggable={false}><Typography sx={linkStyles}>About</Typography></Link>
-              <Link component={ReactLink} to="/contact" draggable={false}><Typography sx={linkStyles}>Contact</Typography></Link>
-            </Stack>
-          </Grid>
-
-          <Grid item xs={12}>
-            <SocialLinks/>
-          </Grid>
+      <Grid container sx={{height: "100%", px: 5}}>
+        <Grid item mt={11} xs={12} sm={6}>
+          <Typography variant="h1" sx={nameStyles}>Paolo</Typography>
+          <Typography variant="h1" sx={nameStyles}>Bugarin</Typography>
+          <Typography 
+            variant="h5"
+            sx={{
+              mt:8,
+              fontWeight: 300, 
+              opacity: .75, 
+              mixBlendMode: "difference", 
+              letterSpacing: 2
+            }}
+            
+          >
+            {`< `}FULL STACK DEVELOPER{` />`}
+          </Typography>
         </Grid>
+
+        <Grid item xs={12} sm={6}>
+          <Stack spacing={1.5} alignItems="flex-end" justifyContent="center" pt={20} sx={{height: "100%"}}>
+            <Box sx={{border: 1, mixBlendMode: "difference"}} my={4}>
+              <IconButton size="large" onClick={() => navigate("/home")} >
+                <ArrowForwardIosIcon fontSize="medium" color="primary"/>
+              </IconButton>
+            </Box>
+            <Link component={ReactLink} to="/home" draggable={false}><Typography sx={linkStyles}>Home</Typography></Link>
+            <Link component={ReactLink} to="/projects" draggable={false}><Typography sx={linkStyles}>Projects</Typography></Link>
+            <Link component={ReactLink} to="/about" draggable={false}><Typography sx={linkStyles}>About</Typography></Link>
+            <Link component={ReactLink} to="/contact" draggable={false}><Typography sx={linkStyles}>Contact</Typography></Link>
+          </Stack>
+        </Grid>
+
+        <Grid item xs={12}>
+          <SocialLinks/>
+        </Grid>
+      </Grid>
     </PageContainer>
   )
 }
