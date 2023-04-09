29cfa010a8b5d42a3ca4813dc09b3624b4ab22af
paoloBugarin <paolopaolobugarin@gmail.com>
2023-04-08 22:13:08 -0700

    updated aboutnavigation component

--- src/pages/About/AboutNavigation.jsx
+++ src/pages/About/AboutNavigation.jsx
@@ -1,9 +1,38 @@
 import React from 'react'
 import { Box, List, ListItemText, ListItemButton  } from '@mui/material'
 
+const MOCK_OPTIONS = [
+  {name: "achievements", title: "Achievements"},
+  {name: "education", title: "Education"},
+  {name: "background", title: "Background"},
+]
 const AboutNavigation = () => {
+  const handleClick = (id) => {
+    const section = document.querySelector(id);
+    section.scrollIntoView( { behavior: 'instant', block: 'center' } );
+  };
+
   return (
-    <div>AboutNavigation</div>
+    <List dense>
+      <Box sx={{height: "4px", width: "10px", my: 2}}>
+        <Box sx={{background: "white", height: "100%", width: "100%"}}></Box>
+      </Box>
+      {MOCK_OPTIONS.map(item => <ListItemButton key={item.name} sx={{mb:.5, px: .5, position: "relative"}} onClick={() => handleClick(`#${item.name}`)}>
+        <ListItemText 
+          primaryTypographyProps={{
+            variant: "body1", 
+            fontSize: 13.5, 
+            fontWeight: 300, 
+            letterSpacing: .15,
+            color: "secondary",
+          }}
+          sx={{
+            opacity: 1,
+          }}
+          >{item.title}
+        </ListItemText>
+      </ListItemButton>)}
+    </List>
   )
 }
 
