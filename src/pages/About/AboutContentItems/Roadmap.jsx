d894bf0c319ca143337fd6f7ddc95143b3813b5d
paoloBugarin <paolopaolobugarin@gmail.com>
2023-04-11 23:14:11 -0700

    updated about page, installed mui/lab package

--- src/pages/About/AboutContentItems/Roadmap.jsx
+++ src/pages/About/AboutContentItems/Roadmap.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Grid, Typography, Box } from '@mui/material'
+import { Grid, Typography, Box, Paper } from '@mui/material'
+import { Timeline, TimelineItem, TimelineSeparator, TimelineDot, TimelineConnector, TimelineContent  } from '@mui/lab';
 
 const boxTextStyles = {
   fontWeight: 300,
@@ -11,9 +12,26 @@ const Roadmap = () => {
     <Grid container sx={{px: 5, justifyContent: "center", alignItems: "center", maxWidth: "1900px", mx: "auto", mt: 5}}>
       
       <Grid item xs={9}>
-        <Typography variant="h5" sx={{mixBlendMode: "difference"}}>My Journey</Typography>
-        <Box sx={{background: "white", height: "4px", width: "6px", mb: 6, mt: 2 }}></Box>
-        <Typography variant="body1" mb={2} fontWeight={100} fontSize={18} letterSpacing={.65} lineHeight={1.7}>
+        <Typography variant="h5" sx={{mixBlendMode: "difference"}} align='center' fontSize="1.75rem">My Development Roadmap</Typography>
+        <Box sx={{background: "white", height: "4px", width: "35px", mb: 6, mt: 3, mx: "auto", mixBlendMode: "difference" }}></Box>
+        <Timeline position="alternate">
+          <TimelineItem>
+            <TimelineSeparator>
+            <TimelineDot color="secondary" />
+            <TimelineConnector />
+            </TimelineSeparator>
+            <TimelineContent>
+              <Paper sx={{p:2, align: "right", m:2}}>
+              <Typography variant="body1" mb={2} fontWeight={100} fontSize={18} letterSpacing={.65} lineHeight={1.7}>
+                Before my development journey, I studied <Box component="span" fontWeight={200}>Architecture</Box> back in Philippines where I learned about 
+                <Box component="span" color="primary.light" fontWeight={200}> design planning, techniques, and strategies</Box> that I continue to incorporate through my projects. 
+              </Typography>
+              </Paper>
+            </TimelineContent>
+          </TimelineItem>
+        </Timeline>
+
+        {/* <Typography variant="body1" mb={2} fontWeight={100} fontSize={18} letterSpacing={.65} lineHeight={1.7}>
           Before my development journey, I studied <Box component="span" fontWeight={200}>Architecture</Box> back in Philippines where I learned about 
           <Box component="span" color="primary.light" fontWeight={200}> design planning, techniques, and strategies</Box> that I continue to incorporate through my projects. 
           I started coding back in 2019 through an online course by Colt Steele on Web Development. 
@@ -25,7 +43,7 @@ const Roadmap = () => {
           While being a part-time college student, I was also self-studying through online courses and youtube tutorials, all 
           while aslo being employed full time in a non-related office job. 
           <Box component="span" color="primary.light" fontWeight={200}> As exhausting as it was, the burnout was worth it for the knowledge I gained.</Box>
-        </Typography>
+        </Typography> */}
       </Grid>
     </Grid>
   )
