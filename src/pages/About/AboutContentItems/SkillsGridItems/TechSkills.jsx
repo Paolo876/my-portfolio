dcab44a27558549a512e68712132bd0bac5288a0
paoloBugarin <paolopaolobugarin@gmail.com>
2023-04-13 22:31:14 -0700

    updated skills component

--- src/pages/About/AboutContentItems/SkillsGridItems/TechSkills.jsx
+++ src/pages/About/AboutContentItems/SkillsGridItems/TechSkills.jsx
@@ -6,16 +6,16 @@ import SkillIconImageItem from '../../../../components/SkillIconImageItem'
 const TechSkills = ({ skills, title}) => {
   return (
     <Box sx={{mb: 10}}>
-      <Typography variant="h5" sx={{mixBlendMode: "difference"}} fontSize="1.20rem">{title}</Typography>
+      <Typography variant="h5" sx={{mixBlendMode: "difference"}} fontSize="1.3rem" fontWeight={700} letterSpacing={4}>{title}</Typography>
       <Box sx={{background: "white", height: "4px", width: "10px", mb: 4, mt: 2, ml: .5, mixBlendMode: "difference" }}></Box>
       <Grid container>
           {skills.map(item => 
             <Grid item xs={1.1} key={item.name} align="center">
               <Paper 
                 sx={{
-                  py: 2.5, 
+                  py: 2.75, 
                   my: 1.5, 
-                  width: "100px",
+                  width: "110px",
                   // height: "125px",
                   userSelect: "none",
                   // opacity: .95,  
@@ -25,14 +25,14 @@ const TechSkills = ({ skills, title}) => {
                 
               >
                 <Box>
-                  <SkillIconImageItem src={item.url} height="50px" width="50px" duration={800} objectFit='scale-down'/>
+                  <SkillIconImageItem src={item.url} height="60px" width="60px" duration={800} objectFit='scale-down'/>
                   <Typography 
                     variant="body2" 
                     sx={{
                       mixBlendMode: "difference", 
-                      fontSize: 12, 
+                      fontSize: 13, 
                       fontWeight: 500, 
-                      mt: 2.5,
+                      mt: 3,
                     }}
                     color="secondary.light"
                   >{item.name}</Typography>
