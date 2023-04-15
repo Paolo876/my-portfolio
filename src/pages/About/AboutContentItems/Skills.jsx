62d773a1dc3fbc23d23c124b0ec91292ad9f36c9
paoloBugarin <paolopaolobugarin@gmail.com>
2023-04-14 22:06:26 -0700

    updated skills component

--- src/pages/About/AboutContentItems/Skills.jsx
+++ src/pages/About/AboutContentItems/Skills.jsx
@@ -20,7 +20,8 @@ const Skills = () => {
   const softwareDevSkillsList = skillsList.filter(item => ["programming"].includes(item.type))
 
   return (
-    <Grid container sx={{p: 5, justifyContent: "center", alignItems: "center", maxWidth: "1900px", mx: "auto", mt: 5}}>
+    <Grid container sx={{p: 5, justifyContent: "center", alignItems: "center", maxWidth: "1900px", mx: "auto", pt: 5, position: "relative"}}>
+      <Box sx={{position: "absolute", top: 0, left: 0, height: "100%", width: "100%", background: "rgba(200,200,200,.15)", mixBlendMode: "overlay", zIndex: -1}}></Box>
       <Box>
         <Typography variant="h5" sx={{mixBlendMode: "difference"}} align='center' fontSize="1.75rem">Skills and Certifications</Typography>
         <Box sx={{background: "white", height: "4px", width: "35px", mb: 10, mt: 3, mx: "auto", mixBlendMode: "difference" }}></Box>
