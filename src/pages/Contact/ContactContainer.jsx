799ffc83875ed7d22a472ddc729ec5bf1b5d273a
paoloBugarin <paolopaolobugarin@gmail.com>
2023-04-17 23:32:15 -0700

    updated contact page ui

--- src/pages/Contact/ContactContainer.jsx
+++ src/pages/Contact/ContactContainer.jsx
@@ -7,7 +7,7 @@ import ContactForm from './ContactForm'
 const ContactContainer = () => {
   return (
     <Box sx={{position: "relative", my: "auto", minHeight: "60vh"}}>
-      <Box sx={{position: "absolute", top: 0, left: 0, height: "100%", width: "100%", background: "white", mixBlendMode: "overlay", opacity: .95, zIndex: -1}}></Box>
+      <Box sx={{position: "absolute", top: 0, left: 0, height: "100%", width: "100%", background: "white", mixBlendMode: "overlay", opacity: 1, zIndex: -1}}></Box>
       <Container maxWidth="xl">
         <Grid container align="center">
           <Grid item xs={6}>
--- src/pages/Contact/ContactDescription.jsx
+++ src/pages/Contact/ContactDescription.jsx
@@ -1,17 +1,23 @@
 import React from 'react'
-import { Box, Typography } from '@mui/material'
+import { Box, Stack, Typography } from '@mui/material'
 
 const ContactDescription = () => {
   return (
-    <Box sx={{my: 3.5, mx: 2, px: 6, py: 3}} align='left'>
-      <Box mb={3}>
-        <Typography variant="h4" sx={{letterSpacing: 2, textTransform: "initial", fontWeight: 800, fontSize: 35 }}>Paolo Bugarin</Typography>
-        <Box sx={{background: "white", height: "3px", width: "15px", my: 1.25}} ></Box>
+    <Stack sx={{py: 3.5, mx: 2, pr: 6, height: "100%"}} align='left' justifyContent="space-between">
+      <Box>
+        <Box mb={3}>
+          <Typography variant="h4" sx={{letterSpacing: 2, textTransform: "initial", fontWeight: 800, fontSize: 35 }}>Contact Me</Typography>
+          <Box sx={{background: "white", height: "3px", width: "15px", my: 1.25}} ></Box>
+        </Box>
+        <Typography sx={{fontWeight: 200, fontSize: 16, opacity: .95, lineHeight: 1.4}}>
+          Thank you for your interest! Please fill out the form or use my contact informations provided to get in touch with me.
+          I am excited to take a leap forward and further hone my knowledge and skills with the right company.
+        </Typography>
       </Box>
-      <Typography sx={{fontWeight: 300, fontSize: 17, opacity: .95, lineHeight: 1.75}}>
-        I am excited to take a leap forward and further hone my knowledge and skills with the right company.
-      </Typography>
-    </Box>
+      <Box>
+        
+      </Box>
+    </Stack>
   )
 }
 
