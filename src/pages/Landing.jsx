dd796beaf322f4043eb978401597a023805011b3
paoloBugarin <paolopaolobugarin@gmail.com>
2023-04-19 00:42:15 -0700

    updated landing page ui

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -9,7 +9,7 @@ import DocumentHead from '../components/DocumentHead';
 
 
 const linkStyles = {
-  fontSize: { sm: "1.3rem", md: "1.5rem", lg: "1.6rem", xl:"1.8rem" },
+  fontSize: { xs: "1.2rem", sm: "1.3rem", md: "1.5rem", lg: "1.6rem", xl:"1.8rem" },
   fontWeight: 400,
   textAlign: "right",
   textTransform: "none",
@@ -21,11 +21,11 @@ const linkStyles = {
 }
 
 const nameStyles = {
-  opacity: .9, 
+  opacity: .95, 
   mixBlendMode: "difference", 
   textTransform: "none",
   fontWeight: 800,
-  fontSize: { xs:"5.25rem", sm: "7rem", md: "8rem", lg: "10rem", xl:"11rem" },
+  fontSize: { xs:"5.4rem", sm: "7rem", md: "8rem", lg: "10rem", xl:"11rem" },
   lineHeight: .85,
   letterSpacing: .1,
   userSelect: "none"
@@ -35,13 +35,13 @@ const Landing = () => {
   const navigate = useNavigate();
 
   return (
-    <Container maxWidth="xl" sx={{pt: 8, height: "90vh"}}>
+    <Container maxWidth="xl" sx={{pt: 8, height: "92vh"}}>
       <DocumentHead
         title="Paolo Bugarin | Developer"
         description="I am a full stack web developer based in Los Angeles, California"
         keyword="react, reactjs, redux, next, nextjs, node, nodejs, mern, express, expressjs, mongodb, sequelize, mysql, developer, fullstack developer, full stack developer, javascript"
       />
-      <Grid container sx={{px: {xs: .5, md: 2}, height: "100%" }}>
+      <Grid container sx={{px: {xs: .15, md: 2}, height: "100%" }}>
         <Grid item pt={{xs: 7, md:11}} xs={12} sm={8} sx={{minHeight: {xs: "fit-content", md:"70vh"}}}>
           <Typography variant="h1" sx={nameStyles}>Paolo</Typography>
           <Typography variant="h1" sx={nameStyles}>Bugarin</Typography>
@@ -50,7 +50,7 @@ const Landing = () => {
             sx={{
               mt:{ xs: 4, sm: 5, md: 5, lg: 7, xl:10 } ,
               fontWeight: 200, 
-              fontSize: { xs: "1.1rem", sm: "1.3rem", md: "1.3rem", lg: "1.4rem", xl:"1.5rem" },
+              fontSize: { xs: "1.2rem", sm: "1.3rem", md: "1.3rem", lg: "1.4rem", xl:"1.5rem" },
               opacity: .75, 
               mixBlendMode: "difference", 
               letterSpacing: { xs: .5, md: 1.25, lg: 1.25, xl:1.25 },
@@ -76,7 +76,7 @@ const Landing = () => {
             <Link component={ReactLink} to="/contact" draggable={false}><Typography sx={linkStyles}>Contact</Typography></Link>
           </Stack>
         </Grid>
-        <Grid xs={12} item sx={{ mt: "auto", align: "left"}}>
+        <Grid xs={12} item sx={{ mt: "auto", align: "left"}} pb={2}>
           <SocialLinks />
         </Grid>
       </Grid>
