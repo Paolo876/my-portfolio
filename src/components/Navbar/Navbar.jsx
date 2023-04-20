46865f938c65f1136289d1898f1028ea136b14f2
paoloBugarin <paolopaolobugarin@gmail.com>
2023-04-19 23:59:28 -0700

    updated navbar media queries

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,17 +8,23 @@ const Navbar = () => {
   const location = useLocation();
   const isLandingPageView = Boolean(location.pathname === "/")
 
+  // window.onscroll = () => {
+  //   if(window.pageYOffset == 0) {
+  //     console.log("Top of the window");
+  //   }
+  // }
+
   return (
     <AppBar component="nav" className="navbar" position="fixed">
       <Container maxWidth="xl">
-        <Toolbar>
+        <Toolbar sx={{display: "flex", flexDirection: {xs: "column", md:"row"}, gap: 1, p:{xs:0, md: "initial"}}} >
           <Link component={ReactLink} to="/"><Typography fontWeight={700}>LOGO_HERE</Typography></Link>
           {!isLandingPageView &&
-            <Box sx={{ml: "auto", display: "flex", flexDirection: "row", gap: 2}} >
-              <Link component={NavLink} to="/home" draggable={false}><Typography>Home</Typography></Link>
-              <Link component={NavLink} to="/projects" draggable={false}><Typography>Projects</Typography></Link>
-              <Link component={NavLink} to="/about" draggable={false}><Typography>About</Typography></Link>
-              <Link component={NavLink} to="/contact" draggable={false}><Typography>Contact</Typography></Link>
+            <Box sx={{ml: {xs:0, md:"auto"}, display: "flex", flexDirection: "row", gap: 3}} >
+              <Link component={NavLink} to="/home" draggable={false}><Typography fontSize={{xs: 14, sm: 15, md: 16}}>Home</Typography></Link>
+              <Link component={NavLink} to="/projects" draggable={false}><Typography fontSize={{xs: 14, sm: 15, md: 16}}>Projects</Typography></Link>
+              <Link component={NavLink} to="/about" draggable={false}><Typography fontSize={{xs: 14, sm: 15, md: 16}}>About</Typography></Link>
+              <Link component={NavLink} to="/contact" draggable={false}><Typography fontSize={{xs: 14, sm: 15, md: 16}}>Contact</Typography></Link>
             </Box>
           }
         </Toolbar>
