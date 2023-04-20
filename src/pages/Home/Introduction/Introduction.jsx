8a9e9a1db54557d39a56e056cef14d3d14206dd8
paoloBugarin <paolopaolobugarin@gmail.com>
2023-04-19 21:41:00 -0700

    updated introduction component media queries

--- src/pages/Home/Introduction/Introduction.jsx
+++ src/pages/Home/Introduction/Introduction.jsx
@@ -1,16 +1,15 @@
 import React from 'react'
 import { Grid, Box, Typography } from '@mui/material'
 import DescriptionGridItem from './DescriptionGridItem'
-import SocialLinks from '../../../components/SocialLinks'
 
 
 const Introduction = () => {
   return (
-    <Grid container sx={{px: 5, minHeight: "85vh", justifyContent: "center", alignItems: "center", maxWidth: "1900px", mx: "auto"}}>
-      <Grid item xs={12} md={7.5} py={4} order={{xs: 2, md: 1}}>
+    <Grid container sx={{px: {xs:1, sm: 1.5, md: 2, lg:5}, minHeight: "88vh", justifyContent: "center", alignItems: "center", maxWidth: "1900px", mx: "auto", pt: 8}}>
+      <Grid item xs={12} md={7} py={4} order={{xs: 2, md: 1}}>
         <DescriptionGridItem/>
       </Grid>
-      <Grid item xs={12} md={2} pt={4} order={{xs: 1, md: 2}}>
+      <Grid item xs={12} md={2} mt={4} order={{xs: 1, md: 2}}>
         <Box 
           sx={{
             border: 3, 
