44c9f93d187ca0a1e007d52a974ad6a4444ad3af
paoloBugarin <paolopaolobugarin@gmail.com>
2023-04-20 23:34:25 -0700

    updated contactInformations component

--- src/pages/Home/Contact/ContactInformations.jsx
+++ src/pages/Home/Contact/ContactInformations.jsx
@@ -9,7 +9,7 @@ import PersonPinIcon from '@mui/icons-material/PersonPin';
 const ContactInformations = () => {
   return (
     <Stack sx={{mt: 2, pl: {xs:0, md: 2}, gap: 2}}>
-        <Paper variant="outlined" sx={{p: {xs:1.5, lg: 2}, width:"100%", display: "flex", alignItems: "center"}}>
+        <Paper variant="outlined" sx={{p: {xs:1.5, lg: 2}, width:"100%", display: "flex", alignItems: "center", background: "none"}}>
           <PersonPinIcon sx={{mr: {xs: 1.5, lg: 2}, fontSize: { xs:28, sm: 30, md: 30, lg: 35 }}}/>
           <Divider orientation="vertical" flexItem></Divider>
           <Stack ml={{xs: 1.5, lg: 2}}>
@@ -17,7 +17,7 @@ const ContactInformations = () => {
             <Typography variant="body1" sx={{fontSize: {xs:14, sm: 16, md: 16, lg: 20}, fontWeight: 200, mt: .5}}>Los Angeles, CA <PlaceIcon fontSize="inherit"/></Typography>
           </Stack>
         </Paper>
-        <Paper variant="outlined" sx={{p: {xs:1.5, lg: 2}, width:"100%", display: "flex", alignItems: "center"}}>
+        <Paper variant="outlined" sx={{p: {xs:1.5, lg: 2}, width:"100%", display: "flex", alignItems: "center", background: "none"}}>
           <IconButton size="small" href="tel:3234817852" sx={{mixBlendMode: "difference", mr: {xs: 1.5, lg: 2}, p: 0}} disableFocusRipple disableRipple disableTouchRipple>
             <LocalPhoneIcon color="primary" sx={{fontSize: { xs:28, sm: 30, md: 30, lg: 35 }}}/>
           </IconButton> 
@@ -27,7 +27,7 @@ const ContactInformations = () => {
             <Typography variant="body1" sx={{fontSize: {xs:18, sm: 20, md: 18, lg: 25}, letterSpacing: 1, mt: .5}} fontWeight={400}>(323) 481 - 7852</Typography>                
           </Stack>
         </Paper>
-        <Paper variant="outlined" sx={{p: {xs:1.5, lg: 2}, width:"100%", display: "flex", alignItems: "center"}}>
+        <Paper variant="outlined" sx={{p: {xs:1.5, lg: 2}, width:"100%", display: "flex", alignItems: "center", background: "none"}}>
           <IconButton size="small" target="_blank" href="mailto: paolopaolobugarin@gmail.com" sx={{mixBlendMode: "difference", mr: {xs: 1.5, lg: 2}, p: 0}} disableFocusRipple disableRipple disableTouchRipple>
             <MailOutlineIcon color="primary" sx={{fontSize: { xs:28, sm: 30, md: 30, lg: 35 }}}/>
           </IconButton> 
