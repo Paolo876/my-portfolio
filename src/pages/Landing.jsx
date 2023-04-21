bd086238c8129413dba7859014f3371cbc287f31
paoloBugarin <paolopaolobugarin@gmail.com>
2023-04-21 00:00:56 -0700

    code cleanup

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -24,7 +24,7 @@ const nameStyles = {
   mixBlendMode: "difference", 
   textTransform: "none",
   fontWeight: 800,
-  fontSize: { xs:"5.4rem", sm: "7rem", md: "8rem", lg: "10rem", xl:"11rem" },
+  fontSize: { xs:"5.6rem", sm: "7rem", md: "8rem", lg: "10rem", xl:"11rem" },
   lineHeight: .85,
   letterSpacing: .1,
   userSelect: "none"
@@ -34,13 +34,13 @@ const Landing = () => {
   const navigate = useNavigate();
 
   return (
-    <Container maxWidth="xl" sx={{pt: 8, height: "93vh"}}>
+    <Container maxWidth="xl" sx={{pt: 8, height: "93vh", px:0}}>
       <DocumentHead
         title="Paolo Bugarin | Developer"
         description="I am a full stack web developer based in Los Angeles, California"
         keyword="react, reactjs, redux, next, nextjs, node, nodejs, mern, express, expressjs, mongodb, sequelize, mysql, developer, fullstack developer, full stack developer, javascript"
       />
-      <Grid container sx={{px: {xs: .15, md: 2}, height: "100%" }}>
+      <Grid container sx={{px: {xs: 1, md: 2}, height: "100%" }}>
         <Grid item pt={{xs: 7, md:11}} xs={12} sm={8} sx={{minHeight: {xs: "fit-content", md:"70vh"}}}>
           <Typography variant="h1" sx={nameStyles}>Paolo</Typography>
           <Typography variant="h1" sx={nameStyles}>Bugarin</Typography>
@@ -63,7 +63,7 @@ const Landing = () => {
         </Grid>
 
         <Grid item xs={12} sm={4} mt="auto">
-          <Stack spacing={1.55} alignItems="flex-end">
+          <Stack spacing={1.55} alignItems="flex-end" pr={{xs:1, sm:2, md:0}}>
             <Box sx={{border: 0, opacity: .85, display: {xs: "none", md: "initial"}}} pb={6}>
               <IconButton size="large" onClick={() => navigate("/home")} color="secondary">
                 <ArrowForwardIosIcon fontSize="large"/>
