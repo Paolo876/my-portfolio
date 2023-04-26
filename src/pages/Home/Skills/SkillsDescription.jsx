034b5624b710c2927b5289cd944da2d167205561
paoloBugarin <paolopaolobugarin@gmail.com>
2023-04-26 00:12:55 -0700

    updated skills component

--- src/pages/Home/Skills/SkillsDescription.jsx
+++ src/pages/Home/Skills/SkillsDescription.jsx
@@ -8,15 +8,15 @@ import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
 const baseTextStyles = {
   display: "inline", 
   fontWeight: 200, 
-  fontSize: {xs: 13, sm: 14, md: 15, lg: 17}, 
-  lineHeight: 1.75
+  fontSize: {xs: 15, sm: 15, md: 16, lg: 17}, 
+  lineHeight: {xs: 1.4, md: 1.7}
 }
 
 const medTextStyles = {
   display: "inline", 
   fontWeight: 400, 
-  fontSize: {xs: 13.5, sm: 14.5, md: 15.5, lg: 17.5}, 
-  lineHeight: 1.75
+  fontSize: {xs: 15.5, sm: 15.5, md: 16.5, lg: 17.5}, 
+  lineHeight: {xs: 1.4, md: 1.7}
 }
 const SkillsDescription = () => {
   const navigate = useNavigate();
@@ -29,14 +29,14 @@ const SkillsDescription = () => {
         <Typography sx={medTextStyles}>Computer Information Systems </Typography>
         <Typography sx={baseTextStyles}>major in </Typography>
         <Typography sx={medTextStyles}>Web Development and Software Development </Typography>
-        <Typography sx={baseTextStyles}>at Pasadena City College and a big part of my knowledge is also gained from  </Typography>
+        <Typography sx={baseTextStyles}>at Pasadena City College and a part of my knowledge is also gained from  </Typography>
         <Typography sx={medTextStyles}>self-studying </Typography>
         <Typography sx={baseTextStyles}>through </Typography>
-        <Typography sx={medTextStyles}>online courses </Typography>
+        <Typography sx={medTextStyles}>online courses and tutorials,  </Typography>
         <Typography sx={baseTextStyles}>and being a part of </Typography>
         <Typography sx={medTextStyles}>coding groups.</Typography>
       </Box>
-      <Box mt={{xs:.5, md: 1.5}}>
+      {/* <Box mt={{xs:.5, md: 1.5}}>
         <Typography sx={baseTextStyles}>I mainly develop my projects using </Typography>
         <Typography sx={medTextStyles}>ReactJS </Typography>
         <Typography sx={baseTextStyles}>or </Typography>
@@ -46,14 +46,14 @@ const SkillsDescription = () => {
         <Typography sx={baseTextStyles}>and </Typography>
         <Typography sx={medTextStyles}>ExpressJS </Typography>
         <Typography sx={baseTextStyles}>in the Server-Side along with related libraries and frameworks.</Typography>
-      </Box>
-      <Box mt={{xs:4, md: 8}} mb={{xs:2, md: 4}} >
+      </Box> */}
+      <Box mt={{xs:5, md: 8}} mb={{xs:2, md: 4}} >
         <Button 
           variant="contained" 
           color="primary"
           endIcon={<KeyboardArrowRightIcon color="inherit"/>}
           onClick={() => navigate("/projects")}
-          sx={{fontWeight: 500, letterSpacing: 1.5, fontSize: {xs: 11, sm: 13, md: 14, lg: 16}}}
+          sx={{fontWeight: 500, letterSpacing: 1.5, fontSize: {xs: 13, sm: 14, md: 15, lg: 16}}}
         >
           Explore My Projects
         </Button>
