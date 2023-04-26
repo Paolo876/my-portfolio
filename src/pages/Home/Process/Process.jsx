3f6e5468a1af7a527a8a9024f5c57899f18b1dcf
paoloBugarin <paolopaolobugarin@gmail.com>
2023-04-25 22:16:07 -0700

    updated process components

--- src/pages/Home/Process/Process.jsx
+++ src/pages/Home/Process/Process.jsx
@@ -24,17 +24,17 @@ const Process = () => {
     <Box sx={{position: "relative", px: {xs:1, sm: 1, md: 1.25, lg: 2.25, xl: 3}, py: {xs:2, sm: 3, md: 7, lg: 9}}}>
       <Box sx={{position: "absolute", top: 0, left: 0, height: "100%", width: "100%", background: "rgba(220,220,220,.15)", mixBlendMode: "overlay", opacity: 1, zIndex: -1}}></Box>
       <Box sx={{display: {xs: "initial", lg: "none"}}}>
-        <Typography variant="h4" align="center" fontSize={{xs:22, sm: 25, md: 28}} letterSpacing={{xs:2, sm:3, md:3.5}} lineHeight={{xl:1.3}}>My Work Process</Typography>
+        <Typography variant="h4" align="center" fontSize={{xs:22, sm: 25, md: 32}} letterSpacing={{xs:2, sm:3, md:3.5}} lineHeight={{xl:1.3}}>My Work Process</Typography>
         <Box sx={{background: "rgba(239,235,229,1)", height: "3px", width: "12px", mx: "auto", mt: {xs: 1.5, md:2}, mb:{xs: 4, sm: 4, md:7}}}></Box>
       </Box>
       <Grid container sx={{justifyContent: "center", alignItems: "flex-start", maxWidth: "1500px", mx: "auto", overflowX:"auto", overflowY: "hidden"}}>
-        <Grid item xs={12} md={12} lg={2}>
-          <Box sx={{display: {xs: "none", lg: "initial"}}}>
-            <Typography variant="h4" align="left" fontSize={{xs:35, sm: 40, md: 28, lg: 38, xl: 45}} letterSpacing={{xs:4, sm:6, md:1.5, lg:2, xl:5}} lineHeight={{lg:1.75, xl:1.6}}>My<br/>Work<br/>Process</Typography>
+        <Grid item xs={12} md={12} lg={2.5}>
+          <Box sx={{display: {xs: "none", lg: "initial"}, mixBlendMode: "difference"}}>
+            <Typography variant="h4" align="left" fontSize={{lg: 45, xl: 58}} letterSpacing={{xs:4, sm:6, md:1.5, lg:2, xl:5}} lineHeight={{lg:1.6, xl:1.45}}>My<br/>Work<br/>Process</Typography>
             <Box sx={{background: "rgba(239,235,229,1)", height: "8px", width: "12px", mr: "auto", mt: {xs: 1.5, md:2}, ml: .2}} ></Box>
           </Box>
         </Grid>
-        <Grid item xs={12} md={12} lg={10} pl={{md:0, lg:1, xl: 2}}>
+        <Grid item xs={12} md={12} lg={9.5} pl={{md:0, lg:2, xl: 4}}>
           <Box sx={{display: {xs: "none", md: "initial"}}}>
             <ProcessStepper processItems={processItems}/>
           </Box>
--- src/pages/Home/Process/ProcessStepper.jsx
+++ src/pages/Home/Process/ProcessStepper.jsx
@@ -12,32 +12,29 @@ const ColorlibConnector = styled(StepConnector)(({ theme }) => ({
   [`&.${stepConnectorClasses.alternativeLabel}`]: {
     top: 25,
   },
-  [`&.${stepConnectorClasses.active}`]: {
-    [`& .${stepConnectorClasses.line}`]: {
-      backgroundImage:
-        'linear-gradient( 95deg,rgb(242,113,33) 0%,rgb(233,64,87) 50%,rgb(138,35,135) 100%)',
-    },
-  },
-  [`&.${stepConnectorClasses.completed}`]: {
-    [`& .${stepConnectorClasses.line}`]: {
-      backgroundImage:
-        'linear-gradient( 95deg,rgb(242,113,33) 0%,rgb(233,64,87) 50%,rgb(138,35,135) 100%)',
-    },
-  },
+  // [`&.${stepConnectorClasses.completed}`]: {
+  //   [`& .${stepConnectorClasses.line}`]: {
+  //     backgroundImage:
+  //       'linear-gradient( 95deg,rgb(242,113,33) 0%,rgb(233,64,87) 50%,rgb(138,35,135) 100%)',
+  //   },
+  // },
   [`& .${stepConnectorClasses.line}`]: {
     height: 2,
     border: 0,
-    opacity:.65, 
+    opacity:.75, 
     backgroundColor:
-      theme.palette.mode === 'dark' ? theme.palette.grey[800] : '#eaeaf0',
+      theme.palette.secondary.dark,
+      // theme.palette.mode === 'dark' ? theme.palette.grey[600] : '#eaeaf0',
     borderRadius: 1,
   },
 }));
 
 
-const ColorlibStepIconRoot = styled('div')(({ theme, ownerState }) => ({
+const ColorlibStepIconRoot = styled('div')(({ theme, ownerState }) => {
+
+  return ({
   // backgroundColor: theme.palette.mode === 'dark' ? theme.palette.grey[700] : '#ccc',
-  // backgroundColor: "theme.palette.primary",
+  backgroundColor: theme.palette.primary.main,
   zIndex: 1,
   // color: '#fff',
   width: 55,
@@ -47,25 +44,19 @@ const ColorlibStepIconRoot = styled('div')(({ theme, ownerState }) => ({
   borderRadius: '50%',
   justifyContent: 'center',
   alignItems: 'center',
-  ...(ownerState.active && {
-    backgroundImage:
-      'linear-gradient( 136deg, rgb(242,113,33) 0%, rgb(233,64,87) 50%, rgb(138,35,135) 100%)',
-    boxShadow: '0 4px 10px 0 rgba(0,0,0,.25)',
-  }),
-  ...(ownerState.completed && {
-    backgroundImage:
-      'linear-gradient( 136deg, rgb(242,113,33) 0%, rgb(233,64,87) 50%, rgb(138,35,135) 100%)',
-  }),
-}));
+  // border: "2px solid white",
+  margin: "0 2px",
+
+})});
 
 
 function ColorlibStepIcon(props) {
   const { active, completed, className } = props;
 
   const icons = {
-    1: <Box sx={{filter: "invert(.95)", p:1.25}}><SkillIconImageItem src={conceptIcon} objectFit='scale-down'/></Box>,
-    2: <Box sx={{filter: "invert(.95)", p:1.5}}><SkillIconImageItem src={planningIcon} objectFit='scale-down'/></Box>,
-    3: <Box sx={{filter: "invert(.95)", p:1.25}}><SkillIconImageItem src={executeIcon} objectFit='scale-down'/></Box>,
+    1: <Box sx={{p:1.25}}><SkillIconImageItem src={conceptIcon} objectFit='scale-down'/></Box>,
+    2: <Box sx={{p:1.5}}><SkillIconImageItem src={planningIcon} objectFit='scale-down'/></Box>,
+    3: <Box sx={{p:1.25}}><SkillIconImageItem src={executeIcon} objectFit='scale-down'/></Box>,
   };
 
   return (
@@ -82,7 +73,7 @@ const ProcessStepper = ({ processItems }) => {
       {processItems.map(item => <Step key={item.title}>
         <StepLabel StepIconComponent={ColorlibStepIcon}>
           <Typography variant="h6" letterSpacing={1.25} fontWeight={800} fontSize={{md:18, lg:20, xl:23}}>{item.title}</Typography>
-          <Typography variant="body1" fontSize={{md: 13.5, lg:14, xl: 15.5}} fontWeight={400} px={{md: .5, lg:1, xl:4}} mt={{md: 2, lg:2, xl: 3.5}} sx={{mixBlendMode: "difference", textAlign: "justify", textJustify: "inter-word", opacity: .9}}>
+          <Typography variant="body1" fontSize={{md: 13.5, lg:14, xl: 15.5}} fontWeight={400} px={{md: .5, lg:1, xl:2.25}} mt={{md: 2, lg:2, xl: 3.5}} sx={{textAlign: "justify", textJustify: "inter-word", opacity: .7}}>
             {item.body}
           </Typography>
         </StepLabel>
