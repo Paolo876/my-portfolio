b616976e21f96efb736a14e8882b43d119510373
paoloBugarin <paolopaolobugarin@gmail.com>
2023-04-30 01:01:59 -0700

    updated contact page ui

--- src/pages/Home/Contact/Contact.jsx
+++ src/pages/Home/Contact/Contact.jsx
@@ -11,7 +11,7 @@ const Contact = () => {
   <Box sx={{position: "relative"}}>
     <Box sx={{position: "absolute", top: 0, left: 0, height: "100%", width: "100%", background: "rgba(0,0,0,.15)", mixBlendMode: "overlay", opacity: 1, zIndex: -1}}></Box>
 
-    <Grid container sx={{px: {xs:0, sm: 0, md: 1.5, lg: 3}, py: {xs:6, sm: 6, md: 8, lg: 10, xl: 8}, justifyContent: "center", alignItems: "flex-start", maxWidth: "1500px", mx: "auto"}}>
+    <Grid container sx={{px: {xs:3, sm: 3, md: 4, lg: 3}, py: {xs:6, sm: 6, md: 8, lg: 10, xl: 8}, justifyContent: "center", alignItems: "flex-start", maxWidth: "1500px", mx: "auto"}}>
       <Grid item xs={12}>
           <Grid container  sx={{justifyContent: "space-between", alignItems: "flex-start" }}>
             <Grid item xs={12} md={6.5}>
@@ -20,7 +20,7 @@ const Contact = () => {
                 <Box sx={{background: "rgba(239,235,229,1)", height: "2px", width: "16px", mr: "auto", mt: {xs: 1, md:1}, mr: .5, mixBlendMode: "difference", opacity: .75}}></Box>
               </Box>
             </Grid>
-            <Grid item xs={12} md={5.5}>
+            <Grid item xs={12} md={5.5} mt={{xs: 5, md: 0}}>
               <Box sx={{width: "100%", height: "100%", display: "flex", justifyContent: "right"}}>
                 <Button endIcon={<DownloadIcon/>} variant="contained" color="secondary" sx={{fontWeight: 500, letterSpacing: 1.5, fontSize: {xs: 11, sm: 12, md: 14, lg: 15, xl: 15}}}>Download My CV</Button>
               </Box>
@@ -29,7 +29,7 @@ const Contact = () => {
         </Grid>
 
       <Grid item xs={12}>
-        <Grid container  sx={{my: 4, justifyContent: "space-between", alignItems: "flex-start" }} spacing={1}>
+        <Grid container  sx={{my: 4, justifyContent: {xs: "center", md:"space-between"}, alignItems: "flex-start" }} spacing={1}>
           <Grid item xs={12} sm={10} md={6} lg={4} order={{xs: 2, md: 1}}>
             <ContactDescription/>
           </Grid>
