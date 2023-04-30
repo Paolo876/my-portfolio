ec8326a013b4559a98ca51cfbe4fb36da8f25048
paoloBugarin <paolopaolobugarin@gmail.com>
2023-04-29 21:28:44 -0700

    updated contact component ui

--- src/pages/Home/Contact/Contact.jsx
+++ src/pages/Home/Contact/Contact.jsx
@@ -8,26 +8,29 @@ import Footer from '../../../components/Footer';
 
 const Contact = () => {
   return (
-    <Grid container sx={{px: {xs:1.5, sm: 3, md: 1.5, lg: 3}, py: {xs:3, sm: 4, md: 5, lg: 10}, justifyContent: "center", alignItems: "flex-start", maxWidth: "1500px", mx: "auto"}}>
+  <Box sx={{position: "relative"}}>
+    <Box sx={{position: "absolute", top: 0, left: 0, height: "100%", width: "100%", background: "rgba(0,0,0,.1)", mixBlendMode: "overlay", opacity: 1, zIndex: -1}}></Box>
+
+    <Grid container sx={{px: {xs:0, sm: 0, md: 1.5, lg: 3}, py: {xs:6, sm: 6, md: 8, lg: 10, xl: 8}, justifyContent: "center", alignItems: "flex-start", maxWidth: "1500px", mx: "auto"}}>
       <Grid item xs={12}>
-        <Grid container  sx={{justifyContent: "space-between", alignItems: "flex-start" }}>
-          <Grid item xs={12} md={6.5}>
-            <Box>
-              <Typography variant="h4" fontSize={{xs:26, sm: 28, md: 30, lg: 32}} letterSpacing={{xs:4, sm:6, md:8, lg:12}}>Let's Talk</Typography>
-              <Box sx={{background: "white", height: "3px", width: "15px", mt: 3}} ></Box>
-            </Box>
-          </Grid>
-          <Grid item xs={12} md={5.5}>
-            <Box sx={{width: "100%", height: "100%", display: "flex", justifyContent: "right", pt: 3}}>
-              <Button endIcon={<DownloadIcon/>} variant="contained" color="secondary" sx={{fontSize: {xs: 11, sm: 13, md: 14, lg: 16}}}>Download My CV</Button>
-            </Box>
+          <Grid container  sx={{justifyContent: "space-between", alignItems: "flex-start" }}>
+            <Grid item xs={12} md={6.5}>
+              <Box>
+                <Typography variant="h4" fontSize={{xs:25, sm: 28, md: 30}} letterSpacing={{xs:3, sm:4, md:6, lg:8}} color="primary.main">Let's Talk</Typography>
+                <Box sx={{background: "rgba(239,235,229,1)", height: "2px", width: "16px", mr: "auto", mt: {xs: 1, md:1}, mr: .5, mixBlendMode: "difference", opacity: .75}}></Box>
+              </Box>
+            </Grid>
+            <Grid item xs={12} md={5.5}>
+              <Box sx={{width: "100%", height: "100%", display: "flex", justifyContent: "right"}}>
+                <Button endIcon={<DownloadIcon/>} variant="contained" color="secondary" sx={{fontWeight: 500, letterSpacing: 1.5, fontSize: {xs: 11, sm: 12, md: 14, lg: 15, xl: 15}}}>Download My CV</Button>
+              </Box>
+            </Grid>
           </Grid>
         </Grid>
-      </Grid>
 
       <Grid item xs={12}>
-        <Grid container  sx={{my: 4, justifyContent: "center", alignItems: "flex-start" }} spacing={1}>
-          <Grid item xs={12} sm={10} md={6} lg={6} order={{xs: 2, md: 1}}>
+        <Grid container  sx={{my: 4, justifyContent: "space-between", alignItems: "flex-start" }} spacing={1}>
+          <Grid item xs={12} sm={10} md={6} lg={4} order={{xs: 2, md: 1}}>
             <ContactDescription/>
           </Grid>
           <Grid item xs={12} sm={10} md={6} lg={6} order={{xs: 1, md: 2}}>
@@ -39,6 +42,7 @@ const Contact = () => {
         <Divider flexItem><SocialLinks gap={1.5} buttonSize="small" flexDirection="row"/></Divider>
       </Grid>
     </Grid>
+  </Box>
   )
 }
 
--- src/pages/Home/Contact/ContactDescription.jsx
+++ src/pages/Home/Contact/ContactDescription.jsx
@@ -10,23 +10,23 @@ const ContactDescription = () => {
 
   return (
     <>
-      <Box mt={{xs:3, sm: 4, md: 5, lg: 6}}>
-        <Typography sx={{fontWeight: 300, fontSize: {xs:15, sm: 17, md: 18, lg: 19}, opacity: .95, lineHeight: 1.5}}>
+      <Box mt={{xs:3, sm: 4, md: 5, lg: 1}}>
+        <Typography variant="body1" fontSize={{xs: 15, sm: 15, md: 15.5, lg:16, xl: 17}} fontWeight={400} sx={{textAlign: {xs: "justify"}, textJustify: "inter-word"}} lineHeight={1.4}>
           I am excited to take a leap forward and further hone my knowledge and skills with the right company.
         </Typography>
-        <Typography sx={{fontWeight: 200, fontSize: {xs:14, sm: 15, md: 16, lg: 17}, opacity: .8, lineHeight: 1.45}} mt={3}>
+        <Typography variant="body1" fontSize={{xs: 14, sm: 14, md: 15, lg:16, xl: 16}} fontWeight={300} sx={{textAlign: {xs: "justify"}, textJustify: "inter-word", opacity: .8}} mt={1} lineHeight={1.4}>
           For any questions or inquiries, please feel free to get in touch with me through my contact informations provided. 
           <br/>You can also reach me through submitting the inquiry form in my contact page.
         </Typography>
 
       </Box> 
-      <Box mt={{xs:4, md: 6, lg: 8}}>
+      <Box mt={{xs:5, md: 6, lg: 8}}>
         <Button 
           variant="outlined" 
           color="primary"
           endIcon={<KeyboardArrowRightIcon color="inherit"/>}
           onClick={() => navigate("/contact")}
-          sx={{fontSize: {xs: 11, sm: 13, md: 14, lg: 16}}}
+          sx={{fontWeight: 500, letterSpacing: 1.5, fontSize: {xs: 11, sm: 12, md: 14, lg: 15, xl: 16}}}
         >
           Contact Page
         </Button>
