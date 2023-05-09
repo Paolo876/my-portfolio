570594dc5497f7745fa339e61fe7115d7bbb26e2
paoloBugarin <paolopaolobugarin@gmail.com>
2023-05-08 21:50:00 -0700

    updated project page ui

--- src/pages/Projects/ProjectItem/ProjectItemPreview.jsx
+++ src/pages/Projects/ProjectItem/ProjectItemPreview.jsx
@@ -3,7 +3,7 @@ import Image from 'mui-image';
 import Carousel from 'react-material-ui-carousel';
 import ProjectImageModal from './ProjectImageModal';
 
-import { Stack, Button, Box } from '@mui/material';
+import { Stack, Button, Box, Typography } from '@mui/material';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import PublicIcon from '@mui/icons-material/Public';
 import "./ProjectItemPreview.scss";
@@ -16,19 +16,27 @@ const ProjectItemPreview = ({ coverImage, images, url, githubUrl, title }) => {
   return (
   <Box className="project-item-preview">
     <Box className="project-item-overlay-container" sx={{p: 2}}>
-      <Stack className="project-item-content" sx={{border: 2, alignItems: "center", justifyContent: "center"}}>
+      <Stack className="project-item-content" sx={{border: 2, alignItems: "center", justifyContent: "center", px: "15%"}}>
         <Box>
-          <Button variant="outlined">
-            Read More About {title}
+          <Button variant="outlined" color="secondary" size="large">
+          Read More About {title}
+            {/* <Typography 
+              variant="body1" 
+              fontSize={{xs: 12, sm: 12.5, md: 13, lg:14, xl: 16}} 
+              fontWeight={600} 
+              p={.75}
+            >
+              Read More About {title}
+            </Typography> */}
           </Button>
         </Box>
-        <Box sx={{background: "rgba(239,235,229,1)", height: 3, width: "30%", mx: "auto", my: 3,  mixBlendMode: "difference", opacity: .5}}></Box>
+        <Box sx={{background: "rgba(239,235,229,1)", height: 2, width: "30%", mx: "auto", my: 4,  mixBlendMode: "difference", opacity: .25}}></Box>
 
         <Box sx={{display: "flex", gap: 3}}>
-          <Button variant="outlined" startIcon={<PublicIcon/>}>
+          <Button variant="contained" startIcon={<PublicIcon/>} color="primary" size="small" sx={{fontWeight: 500}}>
             Web Demo
           </Button>
-          <Button variant="outlined" startIcon={<GitHubIcon/>}>
+          <Button variant="outlined" startIcon={<GitHubIcon/>} color="primary" size="small" sx={{fontWeight: 500}}>
             Git Repo
           </Button>
         </Box>
@@ -45,6 +53,7 @@ const ProjectItemPreview = ({ coverImage, images, url, githubUrl, title }) => {
             src={item.url} 
             fit="cover"
             duration={800}
+            sx={{filter: "brightness(.95)"}}
           />
       )}
     </Carousel>
