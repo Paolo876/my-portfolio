a5447199b85716b51d0f3b47da8d00ed7c6fae41
paoloBugarin <paolopaolobugarin@gmail.com>
2023-05-14 00:52:17 -0700

    updated projects page ui

--- src/pages/Projects/ProjectItem/ProjectItemDescription.jsx
+++ src/pages/Projects/ProjectItem/ProjectItemDescription.jsx
@@ -28,7 +28,9 @@ const ProjectItemDescription = ({ title, briefDescription, technologies, feature
           fontSize={{ xs:24, sm: 24, md: 30, lg: 30, xl: 35 }} 
           letterSpacing={{xs:1, sm:1.25, md: 1.25, lg: 1.5, xl:2}} 
           lineHeight={{xl:1}} 
-          color="primary.main"
+          sx={{mixBlendMode: "difference"}}
+
+          // color="primary.main"
         >
           {title}
         </Typography>
@@ -37,40 +39,30 @@ const ProjectItemDescription = ({ title, briefDescription, technologies, feature
         <Box sx={{pl: .5, pr: 1, mt: {xs: 1.5, lg: 2, xl:3}}}>
           <Typography 
             variant="body1" 
-            fontSize={{xs: 12, sm: 12, md: 13, lg:13.5, xl: 14.5}} 
+            fontSize={{xs: 12, sm: 12, md: 13, lg:13.5, xl: 15}} 
             fontWeight={200} 
+            sx={{mixBlendMode: "difference", opacity: .9}}
           >
             {briefDescription}
           </Typography>
-          <Box mt={3} mb={2}>
-            <Button 
-              variant="outlined" 
-              size="small" 
-              color="primary"
-              sx={{fontWeight: 500, fontSize: {xs: 11, sm: 11, md: 12, lg: 12, xl: 13}, px: 2}}
-              onClick={() => navigate(name)}
-              >Read More
-              <ArrowForwardIosIcon sx={{fontSize: "inherit", ml: 1.25}}/>  
-            </Button>
-          </Box>
         </Box>
       </Box>
 
       <Box sx={{mt:"auto", pl: .5, display: {xs: "none", md: "initial"}}}>
         <Typography              
           variant="h6" 
-          color="secondary.light"
+          // color="secondary.light"
           sx={{
             fontWeight: 700,
             fontSize:{ md: 17, lg:16, xl: 18 },
             textTransform: "none",
-            opacity: .8,
+            mixBlendMode: "difference",
             letterSpacing: .5,
           }}
         >
-          Main Technologies Used: 
+          Main Tech Stack: 
         </Typography>
-        <Grid container sx={{mt:{lg: .5, xl:1.5}}}>
+        <Grid container sx={{mt:{lg: .5, xl:.5}, opacity: .6}}>
           {skillsList && technologies.map(item => {
             const tech = skillsList.find(_item => _item.name === item)
             if(tech) return <Grid item xs={2.05} key={tech.name} align="center" my={.25}>
@@ -79,7 +71,7 @@ const ProjectItemDescription = ({ title, briefDescription, technologies, feature
                 arrow 
                 >
               <Box sx={boxStyles} variant="outlined">
-                <Box height={{xs: 21, sm: 22, md:23, lg:24, xl: 28}} width={{xs: 21, sm: 22, md:23, lg:24, xl: 28}}>
+                <Box height={{xs: 21, sm: 22, md:23, lg:24, xl: 26}} width={{xs: 21, sm: 22, md:23, lg:24, xl: 26}}>
                   <SkillIconImageItem 
                     src={tech.monoUrl} 
                     duration={800} 
@@ -92,6 +84,17 @@ const ProjectItemDescription = ({ title, briefDescription, technologies, feature
             </Grid>
           })}
         </Grid>
+        <Box mt={4}>
+            <Button 
+              variant="outlined" 
+              size="small" 
+              color="primary"
+              sx={{fontWeight: 500, fontSize: {xs: 11, sm: 11, md: 12, lg: 12, xl: 13}, px: 2}}
+              onClick={() => navigate(name)}
+              >Read More
+              <ArrowForwardIosIcon sx={{fontSize: "inherit", ml: 1.25}}/>  
+            </Button>
+          </Box>
       </Box>
     </Stack>
   )
