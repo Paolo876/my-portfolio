f82daa055ea89e5460b5a72745e00c17c7a7713f
paoloBugarin <paolopaolobugarin@gmail.com>
2023-05-15 23:05:00 -0700

    updated project item preview ui

--- src/pages/Projects/ProjectItem/ProjectItemPreview.jsx
+++ src/pages/Projects/ProjectItem/ProjectItemPreview.jsx
@@ -15,47 +15,50 @@ const ProjectItemPreview = ({ coverImage, images, url, githubUrl, title, name, l
   const imagesList = [coverImage, ...images ];
 
   return (
-  <Box className="project-item-preview">
-    <Box className="project-item-overlay-container" sx={{p: {xs:1, md:2}, display: {xs: "none", sm: "initial"}}}>
-      <Stack className="project-item-content" sx={{border: 2, alignItems: "center", justifyContent: "center", px: {xs: "7%", md:"15%"}, borderColor: "rgba(255,255,255,.25)"}}>
-        <Box>
-          <Button 
-            variant="outlined" 
-            color="primary" 
-            endIcon={<ArrowForwardIosIcon/>} 
-            sx={{fontWeight: 500, fontSize:{xs: 11, sm: 12, md:14}}} 
-            onClick={() => navigate(name, { state: {screenView : window.pageYOffset}})}
-          >
-            Read More About {title}
-          </Button>
-        </Box>
- 
-        <Box sx={{background: "rgba(255,255,255,.2)", height: 3, width: "20%", mx: "auto", my: {xs: 2, md:4}}}></Box>
-
-        <Box sx={{display: "flex", gap: {xs: 3, md:5}, pt: .5}}>
-          <Button variant="contained" endIcon={<PublicIcon/>} color="secondary" size="small" sx={{fontWeight: 500, fontSize:{xs: 11, sm: 12, md:13}}} href={url} target='_blank'>
-            Web Demo
-          </Button>
-          <Button variant="outlined" endIcon={<GitHubIcon/>} color="secondary" size="small" sx={{fontWeight: 600, fontSize:{xs: 11, sm: 12, md:13}}} href={githubUrl} target='_blank'>
-            Git Repo
-          </Button>
-        </Box>
-      </Stack>
-    </Box>
+  <Box>
       <Carousel 
-        indicators={false}
         navButtonsAlwaysInvisible={true}
         autoPlay={isInView}
-        stopAutoPlayOnHover={false}
+        stopAutoPlayOnHover={true}
+        interval={3000}
       >
         {imagesList.map((item, index) => 
+          <Box className="project-item-preview">
+            <Box className="project-item-overlay-container" sx={{p: {xs:1, md:2}, display: {xs: "none", sm: "initial"}}}>
+              <Stack className="project-item-content" sx={{border: 2, alignItems: "center", justifyContent: "center", px: {xs: "7%", md:"15%"}, borderColor: "rgba(255,255,255,.25)"}}>
+                <Box>
+                  <Button 
+                    variant="outlined" 
+                    color="primary" 
+                    endIcon={<ArrowForwardIosIcon/>} 
+                    sx={{fontWeight: 500, fontSize:{xs: 11, sm: 12, md:14}}} 
+                    onClick={() => navigate(name, { state: {screenView : window.pageYOffset}})}
+                  >
+                    Read More About {title}
+                  </Button>
+                </Box>
+        
+                <Box sx={{background: "rgba(255,255,255,.2)", height: 3, width: "20%", mx: "auto", my: {xs: 2, md:4}}}></Box>
+
+                <Box sx={{display: "flex", gap: {xs: 3, md:5}, pt: .5}}>
+                  <Button variant="contained" endIcon={<PublicIcon/>} color="secondary" size="small" sx={{fontWeight: 500, fontSize:{xs: 11, sm: 12, md:13}}} href={url} target='_blank'>
+                    Web Demo
+                  </Button>
+                  <Button variant="outlined" endIcon={<GitHubIcon/>} color="secondary" size="small" sx={{fontWeight: 600, fontSize:{xs: 11, sm: 12, md:13}}} href={githubUrl} target='_blank'>
+                    Git Repo
+                  </Button>
+                </Box>
+              </Stack>
+            </Box>
             <Image 
-              src={item.url} 
-              fit="scale-down"
-              duration={400}
-              sx={{filter: "brightness(.95)"}}
-              key={index}
-            />
+                src={item.url} 
+                fit="scale-down"
+                duration={300}
+                sx={{filter: "brightness(.95)"}}
+                key={index}
+              />
+          </Box>
+
         )}
       </Carousel>
       <Box sx={{display: {xs:"flex", md: "none"}, gap: 3, pt: .75, width: "100%", alignItems: "flex-start", justifyContent: "right", opacity: .9}}>
