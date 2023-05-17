3fb86f2266651e931092d85574a06e24b09ee2f3
paoloBugarin <paolopaolobugarin@gmail.com>
2023-05-17 00:10:14 -0700

    updated introduction component

--- src/pages/About/Introduction.jsx
+++ src/pages/About/Introduction.jsx
@@ -9,7 +9,7 @@ const boxTextStyles = {
 
 
 const baseTextStyles = {
-  fontSize:{xs: 12, sm: 12, md: 13, lg:14, xl: 15},
+  fontSize:{xs: 12, sm: 12, md: 13, lg:14, xl: 15.5},
   fontWeight: 300,
   textAlign: "justify", 
   textJustify: "inter-word", 
@@ -28,11 +28,10 @@ const Introduction = () => {
         justifyContent: "center", 
         alignItems: "flex-start", 
         maxWidth: "1500px", 
-
         mx: "auto",
       }}
     >      
-      <Grid item xl={6}>
+      <Grid item xl={5}>
         <Box>
           <Typography 
             variant="h1" 
@@ -47,7 +46,7 @@ const Introduction = () => {
           </Typography>
           <Box sx={{background: "rgba(239,235,229,1)", height: {xs: 2, sm: 8, md: 10}, width: {xs: 12, sm: 8, md: 10}, mr: "auto", mt: {xs: 1.25, sm: 1.5, md:3}, ml: .2, mixBlendMode: "difference", opacity: .5}}></Box>
         </Box>
-        <Box mt={20}>
+        <Box mt={15}>
           <Typography variant="body1" sx={baseTextStyles}>          
             I am a <Box sx={boxTextStyles} component="span">Web Developer </Box> 
             based in <Box sx={boxTextStyles} component="span">Los Angeles, California. </Box>
@@ -56,18 +55,18 @@ const Introduction = () => {
             <Box sx={boxTextStyles} component="span"> NodeJS</Box> - <Box sx={boxTextStyles} component="span">ExpressJS </Box>
             for the Server-Side along with related libraries and frameworks.
           </Typography>
-          <Typography variant="body1" sx={baseTextStyles} mt={2}>          
-            I am diligent and always eager to learn new technologies that will help me expand my skillset and strengthen my knowledge. 
+          <Typography variant="body1" sx={baseTextStyles} mt={1}>          
+            I am diligent and eager to learn new technologies that will help me expand my skillset and strengthen my knowledge. I make sure that there is not a 
+            day that I don't make any progress no matter how small.
+          </Typography>
+          <Typography variant="body1" sx={{...baseTextStyles, fontWeight: 400, opacity: 1}} mt={1.5}>          
+            I may lack enough professional experience but I am confident that I have the necessary knowledge and skills to take my path onto the next level.
           </Typography>
 
-          {/* <Typography variant="body1" sx={baseTextStyles} mt={2}>
-            I believe that an effective design should be lean and straightforward that is why I always try to incorporate a minimalist approach. 
-            The lesser the pieces the more intuitive and easier to understand. Elegance through simplicity.
-          </Typography> */}
         </Box>
       </Grid>
 
-      <Grid item xs={12} lg={4} xl={6} sx={{display: "flex", justifyContent: "right"}}>
+      <Grid item xs={12} lg={4} xl={7} sx={{display: "flex", justifyContent: "right"}}>
         <Box 
           sx={{
             border: 3, 
