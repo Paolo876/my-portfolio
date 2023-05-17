3a4d248461e9a63b8c76b36ac0aa366611e124a8
paoloBugarin <paolopaolobugarin@gmail.com>
2023-05-17 00:24:53 -0700

    updated home page ui

--- src/pages/Home/Introduction/DescriptionGridItem.jsx
+++ src/pages/Home/Introduction/DescriptionGridItem.jsx
@@ -9,18 +9,18 @@ const DescriptionGridItem = () => {
 
   return (
     <Stack >
-      <Box sx={{opacity: 1}}>
+      <Box sx={{opacity: .95, mixBlendMode: "difference"}}>
         <Typography sx={{display: "inline", fontWeight: 200, fontSize: {xs: 20, sm: 22, md: 30, lg:36, xl: 45}, mixBlendMode: "difference"}}>I am </Typography>
-        <Typography sx={{display: "inline", fontWeight: 800, fontSize: {xs: 37, sm: 50, md: 56, lg:62, xl: 75}, ml: {xs:.25, sm:1}, lineHeight: 1.25, letterSpacing: {xs:.5, md: 1}, fontFamily: "Manrope"}} color="primary.main">Paolo Bugarin</Typography>
+        <Typography sx={{display: "inline", fontWeight: 800, fontSize: {xs: 37, sm: 50, md: 56, lg:62, xl: 75}, ml: {xs:.25, sm:1}, lineHeight: 1.25, letterSpacing: {xs:.5, md: 1}, fontFamily: "Manrope"}}>Paolo Bugarin</Typography>
         <Typography sx={{display: "inline", fontWeight: 200, fontSize:  {xs: 20, sm: 22, md: 30, lg:36, xl: 45}, mixBlendMode: "difference"}}>,</Typography>
       </Box>
-      <Box sx={{mixBlendMode: "difference", opacity:.9, mt: {sm:1, md:1.25, lg: 2}}}>
+      <Box sx={{mixBlendMode: "difference", opacity:.6, mt: {sm:1, md:1.25, lg: 2}}}>
         <Typography sx={{display: "inline", fontWeight: 100, fontSize: {xs: 15, sm: 15.5, md: 17, lg:18, xl: 21}, lineHeight: 1.2, }}>a  </Typography>
         <Typography sx={{display: "inline", fontWeight: 400, fontSize: {xs: 15, sm: 15.5, md: 18, lg:20, xl: 24}, lineHeight: 1.2, fontFamily: "Manrope"}}>Full Stack Web Developer </Typography>
         <Typography sx={{display: "inline", fontWeight: 100, fontSize: {xs: 15, sm: 15.5, md: 17, lg:18, xl: 21}, lineHeight: 1.2, }}>based in </Typography>
         <Typography sx={{display: "inline", fontWeight: 400, fontSize: {xs: 15, sm: 15.5, md: 18, lg:20, xl: 24}, lineHeight: 1.2, fontFamily: "Manrope"}}>Los Angeles, California.</Typography>
       </Box>
-      <Box mt={{xs: 2.5, sm: 4, md: 6, lg: 5, xl: 6}}>
+      <Box mt={{xs: 3, sm: 4, md: 6, lg: 7, xl: 8}} sx={{opacity: .9}}>
         <Button 
           variant="outlined" 
           endIcon={<KeyboardArrowRightIcon color="inherit"/>}
--- src/pages/Home/SubIntroduction.jsx
+++ src/pages/Home/SubIntroduction.jsx
@@ -18,15 +18,13 @@ const SubIntroduction = () => {
       <Grid item xs={0} sm={3} md={5} lg={5}></Grid>
       <Grid item xs={12} sm={9} md={7} lg={7} mt="auto">
         <Box align="right">
-          <Typography sx={{display: "inline", fontWeight: 400, fontSize: {xs: 13.5, sm: 16, md: 19, lg:22, xl: 26}, lineHeight: 1.35}}>I design and develop websites from a </Typography>
-          <Typography sx={{display: "inline", fontWeight: 500, fontSize: {xs: 13.5, sm: 16, md: 19, lg:22, xl: 26}, lineHeight: 1.35}} >simple static page </Typography>
-          <Typography sx={{display: "inline", fontWeight: 400, fontSize: {xs: 13.5, sm: 16, md: 19, lg:22, xl: 26}, lineHeight: 1.35}}>to a </Typography>
-          <Typography sx={{display: "inline", fontWeight: 500, fontSize: {xs: 13.5, sm: 16, md: 19, lg:22, xl: 26}, lineHeight: 1.35}} >complex dynamic web application </Typography>
-          <Typography sx={{display: "inline", fontWeight: 400, fontSize: {xs: 13.5, sm: 16, md: 19, lg:22, xl: 26}, lineHeight: 1.35}}> using </Typography>
-          <Typography sx={{display: "inline", fontWeight: 500, fontSize: {xs: 13.5, sm: 16, md: 19, lg:22, xl: 26}, lineHeight: 1.35}} color="secondary.light"> ReactJS</Typography>
-          <Typography sx={{display: "inline", fontWeight: 100, fontSize: {xs: 13.5, sm: 16, md: 19, lg:22, xl: 26}, lineHeight: 1.35}}>||</Typography>
-          <Typography sx={{display: "inline", fontWeight: 500, fontSize: {xs: 13.5, sm: 16, md: 19, lg:22, xl: 26}, lineHeight: 1.35}} color="secondary.light">NextJS </Typography>
-          <Typography sx={{display: "inline", fontWeight: 400, fontSize: {xs: 13.5, sm: 16, md: 19, lg:22, xl: 26}, lineHeight: 1.35}}>framework.</Typography>
+          <Typography variant="body1" sx={{display: "inline", fontWeight: 400, fontSize: {xs: 13.5, sm: 16, md: 19, lg:22, xl: 24}, lineHeight: 1.45, mixBlendMode: "difference"}}>
+            I design and develop websites from a 
+            <Box component="span" fontWeight={500}> simple static page </Box> to a 
+            <Box component="span" fontWeight={500}> complex dynamic web application </Box> using 
+            <Box component="span" fontWeight={500} color="primary.light"> ReactJS </Box> <Box component="span" fontWeight={100}> || </Box> 
+            <Box component="span" fontWeight={500} color="primary.light"> NextJS </Box> framework.
+          </Typography>
         </Box>
       </Grid>
     </Grid>
