bfd82de2e8d887c94f94835fb0cef901c6ffba9b
paoloBugarin <paolopaolobugarin@gmail.com>
2023-05-17 23:45:00 -0700

    updated works page component

--- src/pages/Home/Works/Works.jsx
+++ src/pages/Home/Works/Works.jsx
@@ -1,42 +1,47 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom';
-import { Grid, Box, Typography, Button } from '@mui/material'
+import { Grid, Box, Typography, Button, Stack } from '@mui/material'
 import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
 
 const Works = () => {
   const navigate = useNavigate();
 
   return (
-    <Box sx={{position: "relative"}}>
-      <Box sx={{position: "absolute", top: 0, left: 0, height: "100%", width: "100%", background: "rgba(255,255,255,.5)", mixBlendMode: "overlay", opacity: 1, zIndex: -1}}></Box>
-      <Grid container sx={{px: {xs:0, sm: 0, md: 1.5, lg: 3, xl:0}, py: {xs:8, sm: 9, md: 10, lg: 10, xl: 12}, justifyContent: {xs:"center", md: "space-between"}, alignItems: "flex-start", maxWidth: "1500px", mx: "auto"}}>
-        <Grid item xs={12} md={5} lg={7.75}>
+    <Box sx={{position: "relative", height: 350}}>
+      <Box sx={{position: "absolute", top: 0, left: 0, width: "35%", height: "100%"}}>
+        <Box sx={{position: "relative", height: "100%", width: "100%"}}>
+          <Box sx={{position: "absolute", top: 0, left: 0, background: "rgba(180,180,180,1)", width: "100%", height: "100%", mixBlendMode: "overlay"}}></Box>
+          <Stack alignItems="flex-end" justifyContent="center" sx={{height: "90%"}} pr={8}>
           <Typography 
             variant="h2" 
             fontWeight={800} 
-            fontSize={{xs:32, sm: 32, md: 42, lg: 40, xl: 60}} 
+            fontSize={{xs:32, sm: 32, md: 42, lg: 40, xl: 65}} 
             letterSpacing={{xs:2, sm:2, md: 2, lg: 2, xl:3.5}} 
-            lineHeight={1.1} 
-            sx={{opacity: .9}}
+            lineHeight={1.15} 
+            sx={{opacity: .8}}
             
-            color="primary.main"
           >
             Explore<br/>My Works
           </Typography>
-          {/* <Box mt={{xs: 3, sm: 4, md: 6, lg: 7, xl: 8}} sx={{opacity: .9}}>
-            <Button 
-              variant="outlined" 
-              endIcon={<KeyboardArrowRightIcon color="inherit"/>}
-              onClick={() => navigate("/projects")}
-              sx={{fontSize: {xs: 11, sm: 12, md: 14, lg: 15, xl: 16}, letterSpacing: 1.75}}
-            >
-              Projects
-            </Button>
-          </Box> */}
-      </Grid>
-        <Grid item xs={12} md={7} lg={3.75}>
-        </Grid>
-      </Grid>
+        </Stack>
+        </Box>
+      </Box>
+      <Box sx={{position: "absolute", top: 0, right: 0, background: "rgba(0,0,0,.5)", height: "100%", width: "65%", mixBlendMode: "overlay", zIndex: -1, overflow: "hidden", whiteSpace: "nowrap"}}>
+        <Box sx={{position: "relative", height: "100%", width: "100%", transform: "translateX(-200px)"}}>
+          <Box sx={{border: 1, height: 250, width: 400, display: "inline-block", background: "red"}}>
+          </Box>
+          <Box sx={{border: 1, height: 250, width: 400, display: "inline-block", background: "blue"}}>
+          </Box>
+          <Box sx={{border: 1, height: 250, width: 400, display: "inline-block", background: "yellow"}}>
+          </Box>
+          <Box sx={{border: 1, height: 250, width: 400, display: "inline-block", background: "green"}}>
+          </Box>
+          <Box sx={{border: 1, height: 250, width: 400, display: "inline-block", background: "orange"}}>
+          </Box>
+          <Box sx={{border: 1, height: 250, width: 400, display: "inline-block"}}>
+          </Box>
+        </Box>
+      </Box>
     </Box>
   )
 }
