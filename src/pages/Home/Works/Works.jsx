f456bb67bddd1cfffba0092ac6ced464f2504bf5
paoloBugarin <paolopaolobugarin@gmail.com>
2023-05-18 00:20:35 -0700

    created works.scss file

--- src/pages/Home/Works/Works.jsx
+++ src/pages/Home/Works/Works.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { useNavigate } from 'react-router-dom';
 import { Grid, Box, Typography, Button, Stack } from '@mui/material'
 import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
+import "./Works.scss"
+
 
 const Works = () => {
   const navigate = useNavigate();
 
   return (
-    <Box sx={{position: "relative", height: 350}}>
+    <Box sx={{position: "relative", height: 350}} className="works-component">
       <Box sx={{position: "absolute", top: 0, left: 0, width: "35%", height: "100%"}}>
         <Box sx={{position: "relative", height: "100%", width: "100%"}}>
           <Box sx={{position: "absolute", top: 0, left: 0, background: "rgba(180,180,180,1)", width: "100%", height: "100%", mixBlendMode: "overlay"}}></Box>
@@ -26,8 +28,21 @@ const Works = () => {
         </Stack>
         </Box>
       </Box>
-      <Box sx={{position: "absolute", top: 0, right: 0, background: "rgba(0,0,0,.5)", height: "100%", width: "65%", mixBlendMode: "overlay", zIndex: -1, overflow: "hidden", whiteSpace: "nowrap"}}>
-        <Box sx={{position: "relative", height: "100%", width: "100%", transform: "translateX(-200px)"}}>
+      <Box sx={{position: "absolute", top: 0, right: 0, background: "rgba(0,0,0,.5)", height: "100%", width: "65%", overflow: "hidden", whiteSpace: "nowrap"}} className="projects-preview">
+        <Box className="project-overlay">
+            <Box mt={{xs:5, md: 6, lg: 8}} mb={{xs:2, md: 4}} ml={{md: "auto"}} className="overlay-content">
+              <Button 
+                variant="outlined" 
+                color="primary"
+                endIcon={<KeyboardArrowRightIcon color="inherit"/>}
+                onClick={() => navigate("/projects")}
+                sx={{fontWeight: 500, letterSpacing: 1.5, fontSize: {xs: 11, sm: 12, md: 14, lg: 15, xl: 16}}}
+              >
+                Explore My Works
+              </Button>
+            </Box>
+          </Box>
+        <Box sx={{position: "relative", height: "100%", width: "100%", opacity: .8, transform: "translateX(-200px)"}}>
           <Box sx={{border: 1, height: 250, width: 400, display: "inline-block", background: "red"}}>
           </Box>
           <Box sx={{border: 1, height: 250, width: 400, display: "inline-block", background: "blue"}}>
@@ -41,6 +56,7 @@ const Works = () => {
           <Box sx={{border: 1, height: 250, width: 400, display: "inline-block"}}>
           </Box>
         </Box>
+
       </Box>
     </Box>
   )
