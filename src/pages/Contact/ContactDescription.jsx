48862aee6a995d3eeec0a125c22c7e3dc9c3f723
paoloBugarin <paolopaolobugarin@gmail.com>
2023-05-18 23:42:04 -0700

    updated contact page ui

--- src/pages/Contact/ContactDescription.jsx
+++ src/pages/Contact/ContactDescription.jsx
@@ -51,25 +51,25 @@ const ContactDescription = () => {
           <Typography variant="h5" sx={{letterSpacing: 1, textTransform: "none", opacity: .75, fontSize: {xs:12, sm: 13, md: 14, lg: 15}, mt: .5, fontWeight: 200}}>Los Angeles, CA <PlaceIcon sx={{fontSize: "inherit"}} color="primary"/></Typography>
         </Box>
       </Box>
-      <Box sx={{ display: "flex", flexDirection: "row", alignItems: "center", gap: 1, justifyContent: "space-between", opacity: .75 }}>
+      <Box sx={{ display: "flex", flexDirection: "row", alignItems: "center", gap: 1, justifyContent: "space-between"}}>
         <Box>
-          <Typography variant="body1" sx={{letterSpacing: .5, textTransform: "none", fontWeight: 500, opacity: .9}}>Email</Typography>
+          <Typography variant="h5" sx={{letterSpacing: 1, textTransform: "none", fontWeight: 300, opacity: .75, fontSize: {xs:12, sm: 12, md: 13, lg: 14}}}>Email</Typography>
           <Box sx={{background: "white", height: "1px", width: "100%", my: .1, opacity: .15}}></Box>
           <ButtonBase target="_blank" href="mailto: paolopaolobugarin@gmail.com" >
-            <Typography variant="body1" sx={{textTransform: "none", opacity: 1, fontWeight: 500}} color="primary">paolopaolobugarin@gmail.com</Typography>
+            <Typography variant="h6" sx={{letterSpacing: .5, textTransform: "none", opacity: 1, fontWeight: 600, fontSize: {xs:14, sm: 14, md: 15, lg: 16}}} color="primary">paolopaolobugarin@gmail.com</Typography>
           </ButtonBase>
         </Box>
         <Box>
-          <Typography variant="body1" sx={{letterSpacing: .5, textTransform: "none", fontWeight: 500, opacity: .9}}>Phone</Typography>
+          <Typography variant="h5" sx={{letterSpacing: 1, textTransform: "none", fontWeight: 300, opacity: .75, fontSize: {xs:12, sm: 12, md: 13, lg: 14}}}>Phone</Typography>
           <Box sx={{background: "white", height: "1px", width: "100%", my: .1, opacity: .15}}></Box>
           <ButtonBase href="tel:3234817852">
-            <Typography variant="body1" sx={{textTransform: "none", opacity: 1, fontWeight: 500}} color="primary">+1 (323) 481 7852</Typography>
+            <Typography variant="body1" sx={{letterSpacing: .5, textTransform: "none", opacity: 1, fontWeight: 500, fontSize: {xs:14, sm: 14, md: 15, lg: 16}}} color="primary">+1 (323) 481 7852</Typography>
           </ButtonBase>
         </Box>
         <Box>
-          <Typography variant="body1" sx={{letterSpacing: .5, textTransform: "none", fontWeight: 500, opacity: .9}}>Social</Typography>
+          <Typography variant="h5" sx={{letterSpacing: 1, textTransform: "none", fontWeight: 300, opacity: .75, fontSize: {xs:12, sm: 12, md: 13, lg: 14}}}>Social</Typography>
           <Box sx={{background: "white", height: "1px", width: "100%", my: .1, opacity: .15}}></Box>
-          <SocialLinks mixBlendMode='none' buttonSize='medium' iconSize={18} gap={1}/>
+          <SocialLinks mixBlendMode='none' buttonSize='small' iconSize={{ xs: 18, sm: 18, md: 19, lg: 19 }} gap={2.5}/>
         </Box>
       </Box>
     </Stack>
