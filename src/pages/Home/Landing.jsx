905adffcf51c20dc8113bb7b962c74f217a5b10c
paoloBugarin <paolopaolobugarin@gmail.com>
2023-07-07 00:12:19 -0700

    updated landing page ui

--- src/pages/Home/Landing.jsx
+++ src/pages/Home/Landing.jsx
@@ -3,24 +3,22 @@ import { Grid, Typography, Link, Stack, IconButton, Box, Container } from '@mui/
 import { Link as ReactLink } from 'react-router-dom';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import SocialLinks from '../../components/SocialLinks';
-import LocationOnIcon from '@mui/icons-material/LocationOn';
 
 const linkStyles = {
-  fontSize: { xs: "1rem", sm: "1.25rem", md: "1.3rem", lg: "1.3rem", xl:"1.3rem" },
-  my: { xs:1.75, sm: 2 },
-  fontWeight: 300,
+  fontSize: { xs: "1.25rem", sm: "1.5rem", md: "1.6rem", lg: "1.8rem", xl:"2rem" },
+  my: { xs:1.75, sm: 2, md: 2 },
+  fontWeight: 500,
   textAlign: "right",
   textTransform: "none",
-  letterSpacing: {xs: 1.25, md: 1.75},
+  letterSpacing: {xs: 1.75, md: 2, lg: 2.5},
   width: "fit-content",
-  opacity: .7,
-  mixBlendMode: "difference",
-  transition: "all 200ms linear",
+  opacity: .65,
+  transition: "all 150ms ease-in-out",
   fontFamily: "Manrope",
   "&:hover":{
     opacity:1,
-    color: "secondary.light",
-    textShadow: "1px 1px 3px rgba(150,150,150,.15)",
+    textShadow: "1px 1px 3px rgba(100,100,100,.75)",
+    transform: "scale(1.05)"
   }
 }
 
@@ -28,10 +26,10 @@ const nameStyles = {
   textTransform: "none",
   fontWeight: 800,
   fontSize: { xs:"5.6rem", sm: "7rem", md: "8rem", lg: "10rem", xl:"12rem" },
-  lineHeight: 1,
-  letterSpacing: -.25,
+  lineHeight: .9,
+  letterSpacing: -1,
   userSelect: "none",
-  textShadow: "1px 1px 10px rgba(0,0,0,0.25)"
+  textShadow: "1px 1px 3px rgba(100,100,100,.75)",
 }
 
 
@@ -45,16 +43,19 @@ const Landing = () => {
         height: "93vh", 
         position: "relative", 
         px: { xs:0, sm: 1, md: 1, lg: 2.25, xl: 3 }, 
-        py: { xs:8, sm: 10, md: 11, lg: 12, xl: 12 },
+        pt: { xs:8, sm: 12, md: 13, lg: 15, xl: 15 },
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between"
       }}
     >
-      <Grid container sx={{maxWidth: "1500px", mx: "auto", height: "100%", px: {md: 0,lg:2}}}>
+      <Grid container sx={{maxWidth: "1500px", mx: "auto", height: "85%", px: {md: 0,lg:2}}}>
         <Grid item xs={12} sm={8}>
           <Box>
             <Typography variant="h1" sx={nameStyles}>Paolo</Typography>
             <Typography variant="h1" sx={nameStyles}>Bugarin</Typography>
           </Box>
-          <Box mt={{ xs: 4.5, sm: 5, md: 5, lg: 7, xl:12 }} ml={1}>
+          <Box mt={{ xs: 4.5, sm: 5, md: 5, lg: 13, xl:15 }} ml={1}>
             <Typography 
               variant="h5"
               sx={{
@@ -62,7 +63,8 @@ const Landing = () => {
                 fontSize: { xs: "1.2rem", sm: "1.4rem", md: "1.5rem", lg: "1.6rem", xl:"1.8rem" },
                 opacity: .6, 
                 mixBlendMode: "difference", 
-                letterSpacing: { xs: .5, md: 1.25, lg: 1.5, xl:2 },
+                // letterSpacing: { xs: .5, md: 1, lg: 1, xl:.5 },
+                letterSpacing: .25,
                 textTransform: "none",
                 userSelect: "none"
               }}
@@ -71,74 +73,24 @@ const Landing = () => {
             </Typography>
           </Box>
         </Grid>
-        {/* <Grid item pt={{xs: 7, md:11}} xs={12} sm={8} sx={{minHeight: {xs: "fit-content", sm: "65vh",md:"0vh"}}}>
-          <Typography variant="h1" sx={nameStyles}>Paolo</Typography>
-          <Typography variant="h1" sx={nameStyles}>Bugarin</Typography>
-          <Typography 
-            variant="h5"
-            sx={{
-              mt:{ xs: 4.5, sm: 5, md: 5, lg: 7, xl:10 } ,
-              fontWeight: 200, 
-              fontSize: { xs: "1.2rem", sm: "1.4rem", md: "1.5rem", lg: "1.6rem", xl:"1.7rem" },
-              opacity: .7, 
-              mixBlendMode: "difference", 
-              letterSpacing: { xs: .5, md: 1.25, lg: 1.25, xl:1.5 },
-              textTransform: "none",
-              userSelect: "none"
-            }}
-          >
-            {`< `}Full Stack Web Developer{` />`}
-          </Typography>
-          <Typography 
-            variant="h6"
-            sx={{
-              mt:{ xs: 4.5, sm: 5, md: 5, lg: 7, xl:10 } ,
-              fontWeight: 200, 
-              fontSize: { xs: "1.2rem", sm: "1.4rem", md: "1.5rem", lg: "1.6rem", xl:"1.7rem" },
-              opacity: .7, 
-              mixBlendMode: "difference", 
-              letterSpacing: { xs: .5, md: 1.25, lg: 1.25, xl:1.5 },
-              textTransform: "none",
-              userSelect: "none"
-            }}
-          >
-            Los Angeles, CA
-          </Typography>
+        <Grid item xs={12} sm={4} sx={{display: "flex", flexDirection: "column", justifyContent: "space-between", py: 3}}>
+          <Box sx={{width: "fit-content", ml: "auto", opacity: .65}}>
+            <SocialLinks flexDirection='column' color="inherit" gap={.25}/>
+          </Box>
+          <Box align="right">
+            <Link component={ReactLink} to="/projects" draggable={false} sx={{position: "relative"}}><Typography sx={linkStyles}>Projects</Typography></Link>
+            <Link component={ReactLink} to="/about" draggable={false} sx={{position: "relative"}}><Typography sx={linkStyles}>About</Typography></Link>
+            <Link component={ReactLink} to="/contact" draggable={false} sx={{position: "relative"}}><Typography sx={linkStyles}>Contact</Typography></Link>
+          </Box>
         </Grid>
-
-        <Grid item xs={12} sm={4} mt="auto">
-          <Stack spacing={{xs:2.5, md:1.8}} alignItems="flex-end" pr={{xs:1, sm:2, md:0}}>
-            <Box 
-              sx={{
-                border: 1, 
-                opacity: .75, 
-                display: { xs: "none", sm: "initial" },
-                borderColor: "rgba(255,255,255,.35)", 
-                transition: "all 200ms linear",
-
-                "&:hover": { 
-                  borderColor: "rgba(255,255,255,.5)",
-                  opacity: 1, 
-
-                }
-              }} 
-              mb={6}
-            >
-              <IconButton size="large" onClick={() => navigate("/home")} color="primary">
-                <ArrowForwardIosIcon fontSize="large"/>
-              </IconButton>
-            </Box>
-            <Box align="right">
-              <Link component={ReactLink} to="/projects" draggable={false} sx={{position: "relative"}}><Typography sx={linkStyles}>Projects</Typography></Link>
-              <Link component={ReactLink} to="/about" draggable={false} sx={{position: "relative"}}><Typography sx={linkStyles}>About</Typography></Link>
-              <Link component={ReactLink} to="/contact" draggable={false} sx={{position: "relative"}}><Typography sx={linkStyles}>Contact</Typography></Link>
-            </Box>
-          </Stack>
-        </Grid>
-        <Grid xs={12} item sx={{ mt: "auto", align: "left", opacity: .75}} pb={1}>
-          <SocialLinks />
-        </Grid> */}
       </Grid>
+      <Box 
+        sx={{width: "100%", display: "flex", alignItems: "center", justifyContent: "center"}}
+        >
+        <IconButton size="large" onClick={() => navigate("/home")} color="primary" sx={{transform: "rotate(90deg)"}}>
+          <ArrowForwardIosIcon fontSize="large"/>
+        </IconButton>
+      </Box>
     </Box>
   )
 }
