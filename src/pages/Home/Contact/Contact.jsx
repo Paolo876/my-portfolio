be5c107107cc568dd7354e6f3578ef882d60f7b3
paoloBugarin <paolopaolobugarin@gmail.com>
2023-07-12 00:38:00 -0700

    updated contact component

--- src/pages/Home/Contact/Contact.jsx
+++ src/pages/Home/Contact/Contact.jsx
@@ -14,57 +14,22 @@ const Contact = () => {
   <Box sx={{position: "relative"}}>
     <Box sx={{position: "absolute", top: 0, left: 0, height: "100%", width: "100%", background: "rgba(0,0,0,1)", mixBlendMode: "overlay", opacity: .5, zIndex: -1}}></Box>
     <Grid container sx={{px: {xs:3, sm: 3, md: 4, lg: 3}, pt: {xs:3, sm: 4, md: 5, lg: 6, xl: 6}, justifyContent: "center", alignItems: "flex-start", maxWidth: "1500px", mx: "auto"}}>
-      <Grid item xs={12}>
-        <Grid container  sx={{justifyContent: "space-between", alignItems: "center" }}>
-          <Grid item xs={12} md={6}>
-            <Box>
-              <Typography 
-                variant="h2" 
-                fontWeight={400}
-                fontSize={{lg: 42, xl: 45}} 
-                letterSpacing={.5}
-                lineHeight={{lg:1.6, xl:1.1}} 
-                textTransform="none"
-                sx={{
-                  textShadow: "1px 1px 7px rgba(10,10,10,.75)",
-                }}
-              >
-                Let's Talk
-              </Typography>
-            </Box>
-          </Grid>
-          <Grid item xs={12} md={6}>
-            <Box sx={{width: "100%", height: "100%", display: "flex", justifyContent: "right"}}>
-              <ButtonBase 
-                color="inherit"
-                sx={{
-                  boxShadow: 5,
-                  border: 2, 
-                  fontSize: { xs: "1rem", sm: "1rem", md: "1rem", lg: "1rem" },
-                  lineHeight: 1.4, 
-                  borderColor: "rgba(100,100,100,.75)",
-                  px: 2,
-                  py: 1,
-                  fontWeight: 600,
-                  letterSpacing: .5,
-                  opacity: .75,
-                  transition: "all 250ms ease-in-out",
-                  fontFamily: "Manrope",
-                  "&:hover":{
-                    opacity:1,
-                    textShadow: "1px 1px 3px rgba(100,100,100,.75)",
-                    transform: "scale(1.02)",
-                    borderColor: "primary.dark",
-                  }
-                }}
-              >
-                Download My CV <DownloadIcon sx={{ml: 1.5}}/>
-              </ButtonBase>
-            </Box>
-          </Grid>
-        </Grid>
-      </Grid>
-      <Grid item xs={12} lg={4}>
+      <Grid item xs={12} lg={4.5}>
+        <Box>
+          <Typography 
+            variant="h2" 
+            fontWeight={400}
+            fontSize={{lg: 42, xl: 45}} 
+            letterSpacing={.5}
+            lineHeight={{lg:1.6, xl:1.1}} 
+            textTransform="none"
+            sx={{
+              textShadow: "1px 1px 7px rgba(10,10,10,.75)",
+            }}
+          >
+            Let's Talk
+          </Typography>
+        </Box>
         <Box mt={3}>
           <Typography 
             sx={{
@@ -119,7 +84,7 @@ const Contact = () => {
         </Box>
       </Grid>
       
-      <Grid item xs={12} lg={8} sx={{display: "flex", flexDirection: "column", justifyContent: "space-between", alignItems: "center", height: "100%"}}>
+      <Grid item xs={12} lg={4.5} sx={{display: "flex", flexDirection: "column", justifyContent: "space-between", alignItems: "center", height: "100%"}}>
         <Box align="center">
           <Typography 
             variant="h2" 
@@ -155,23 +120,52 @@ const Contact = () => {
               <Typography sx={{fontSize: {xs:14, sm: 14, md: 16, lg: 20}, mt: {xs: 0, sm:.5}}} fontWeight={300}>paolopaolobugarin@gmail.com</Typography>
             </IconButton>
           </Box>
-          <Box mt={2} sx={{width: "fit-content"}}>
-            <SocialLinks 
-              flexDirection='row' 
-              color="inherit" 
-              gap={.25}
-              placement="left"
-              additionalStyles={{
-                opacity: .65,
-                transition: "all 150ms ease-in-out",
-                "&:hover": {
-                  opacity: 1,
-                  transform: "scale(1.15)",
-                  color: "primary.light",
-                }
-              }}
-            />
-          </Box>
+        </Box>
+      </Grid>
+      <Grid item xs={12} lg={3} sx={{display: "flex", flexDirection: "column", justifyContent: "space-between", alignItems: "center", height: "100%"}}>
+        <Box sx={{width: "100%", height: "100%", display: "flex", justifyContent: "right"}}>
+          <ButtonBase 
+            color="inherit"
+            sx={{
+              boxShadow: 5,
+              border: 2, 
+              fontSize: { xs: "1rem", sm: "1rem", md: "1rem", lg: "1rem" },
+              lineHeight: 1.4, 
+              borderColor: "rgba(100,100,100,.75)",
+              px: 2,
+              py: 1,
+              fontWeight: 600,
+              letterSpacing: .5,
+              opacity: .75,
+              transition: "all 250ms ease-in-out",
+              fontFamily: "Manrope",
+              "&:hover":{
+                opacity:1,
+                textShadow: "1px 1px 3px rgba(100,100,100,.75)",
+                transform: "scale(1.02)",
+                borderColor: "primary.dark",
+              }
+            }}
+          >
+            Download My CV <DownloadIcon sx={{ml: 1.5}}/>
+          </ButtonBase>
+        </Box>
+        <Box mt={3} sx={{width: "fit-content"}} ml="auto">
+          <SocialLinks 
+            flexDirection='row' 
+            color="inherit" 
+            gap={.5}
+            placement="bottom"
+            additionalStyles={{
+              opacity: .65,
+              transition: "all 150ms ease-in-out",
+              "&:hover": {
+                opacity: 1,
+                transform: "scale(1.15)",
+                color: "primary.light",
+              }
+            }}
+          />
         </Box>
       </Grid>
     </Grid>
