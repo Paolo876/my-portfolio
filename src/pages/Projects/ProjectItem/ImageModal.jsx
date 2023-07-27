f8474ba776b9733c246a020aa6f8e3240bab29d7
paoloBugarin <paolopaolobugarin@gmail.com>
2023-07-26 23:32:18 -0700

    updated image modal component

--- src/pages/Projects/ProjectItem/ImageModal.jsx
+++ src/pages/Projects/ProjectItem/ImageModal.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import { Box,  } from '@mui/material';
 import Image from 'mui-image';
+import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+
 
 const containerStyles = {
   zIndex:5, 
@@ -9,7 +12,9 @@ const containerStyles = {
   position: "relative", 
   display: "flex", 
   alignItems: "center", 
-  justifyContent: "center",
+  flexDirection: "column",
+  justifyContent: "space-between",
+  pt: "10vh",
   backdropFilter: "blur(2px) contrast(80%) brightness(50%)",
 }
 
@@ -27,9 +32,12 @@ const ImageModal = ({ imageName, imagesList, handleClose }) => {
     <Box sx={containerStyles}>
       <Box sx={{zIndex: 2, height: "100vh", width: "100vw", position: "absolute"}} onClick={handleCloseClick}></Box>
 
-      <Box sx={{zIndex: 3, boxShadow:10, }}>
+      <Box sx={{zIndex: 3, boxShadow:10, maxHeight: "75vh"}}>
         <Image src={image.url} duration={300} fit="scale-down"/>
       </Box>
+
+      {/* actions */}
+      <Box sx={{zIndex: 2, width: "100%", background: "rgba(120, 120, 120, .85)", height: {xl: 100}}}></Box>
     </Box>
   )
 }
