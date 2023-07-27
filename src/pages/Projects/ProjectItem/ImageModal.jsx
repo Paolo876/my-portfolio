84507a37a8ae381f472f2ab26430e9a7b5685728
paoloBugarin <paolopaolobugarin@gmail.com>
2023-07-26 23:55:01 -0700

    updated ImageModal component

--- src/pages/Projects/ProjectItem/ImageModal.jsx
+++ src/pages/Projects/ProjectItem/ImageModal.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Box,  } from '@mui/material';
+import { Box, ButtonBase, IconButton,  } from '@mui/material';
 import Image from 'mui-image';
 import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
@@ -18,6 +18,31 @@ const containerStyles = {
   backdropFilter: "blur(2px) contrast(80%) brightness(50%)",
 }
 
+const actionsContainer = {
+  zIndex: 2, 
+  width: "100%", 
+  background: "rgba(120, 120, 120, .85)", 
+  height: { xl: 150 },
+  display: "flex",
+  ".active": {
+    border: 2,
+    opacity: 1,
+  }
+}
+
+const imageItemStyles = {
+  height: 130, 
+  border: 2, 
+  borderColor: "transparent", 
+  p: .5, 
+  opacity: .5, 
+  transition: "all 250ms ease", 
+  "&:hover": { 
+    opacity: 1, 
+    borderColor: "rgba(50,50,50, .5)",  
+  }
+}
+
 
 const ImageModal = ({ imageName, imagesList, handleClose }) => {
   const image = imagesList.find(item => item.name === imageName)
@@ -32,12 +57,26 @@ const ImageModal = ({ imageName, imagesList, handleClose }) => {
     <Box sx={containerStyles}>
       <Box sx={{zIndex: 2, height: "100vh", width: "100vw", position: "absolute"}} onClick={handleCloseClick}></Box>
 
-      <Box sx={{zIndex: 3, boxShadow:10, maxHeight: "75vh"}}>
+      <Box sx={{zIndex: 3, boxShadow:10, maxHeight: "70vh"}}>
         <Image src={image.url} duration={300} fit="scale-down"/>
       </Box>
 
       {/* actions */}
-      <Box sx={{zIndex: 2, width: "100%", background: "rgba(120, 120, 120, .85)", height: {xl: 100}}}></Box>
+      <Box sx={actionsContainer}>
+        <IconButton color="primary" size="large"><ArrowBackIosNewIcon fontSize='large'/></IconButton>
+        <Box sx={{flex: 1, display: "flex", gap: 2, alignItems: "center", justifyContent: "center"}} >
+          {/* images list here */}
+          {imagesList.map(item => <Box key={item.name}>
+            <ButtonBase 
+              sx={imageItemStyles} 
+              className={item.name === imageName ? "active" : ""}
+            >
+              <Image src={item.url} duration={200} />
+            </ButtonBase>
+          </Box>)}
+        </Box>
+        <IconButton color="primary" size="large"><ArrowForwardIosIcon fontSize='large'/></IconButton>
+      </Box>
     </Box>
   )
 }
