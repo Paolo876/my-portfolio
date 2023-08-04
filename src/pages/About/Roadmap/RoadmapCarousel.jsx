b4338966cd2a36d5923a65bf22705c3c73b6a270
paoloBugarin <paolopaolobugarin@gmail.com>
2023-08-04 00:08:31 -0700

    updated roadmap carousel ui

--- src/pages/About/Roadmap/RoadmapCarousel.jsx
+++ src/pages/About/Roadmap/RoadmapCarousel.jsx
@@ -25,16 +25,53 @@ const roadmapItems = [
 
 const RoadmapCarousel = () => {
   return (
-    <Carousel
-      indicators={true}
-      interval={8000}
-      duration={800} 
-      navButtonsAlwaysInvisible={true}
-    >
-      <Grid container>
-        <Grid item></Grid>
-      </Grid>
-    </Carousel>
+    <Box sx={{py: 5, mt: 4}}>
+      <Carousel
+        indicators={true}
+        interval={8000}
+        duration={800} 
+        navButtonsAlwaysInvisible={true}
+        autoPlay={false}
+      >
+        {roadmapItems.map(item => <Grid container key={item} sx={{minHeight: {xl: 200}}}>
+          <Grid item xs={3}>
+            <Typography
+              variant="h2" 
+              align="left" 
+              fontWeight={400}
+              fontSize={{xs: 22, sm: 23, md: 24, lg: 25, xl: 25}} 
+              letterSpacing={{xs: .5, xl:1}}
+              lineHeight={{xs:1}} 
+              textTransform="none"
+              sx={{
+                textShadow: "1px 1px 7px rgba(25,25,25,.75)",
+              }}
+            >
+              {item.title}
+            </Typography>
+          </Grid>
+          <Grid item xs={9}>
+            <Typography
+              variant='body2'
+              sx={{
+                fontSize: {xs: 12.5, sm: 14.5, md: 14.5, lg:16, xl: 16},
+                fontWeight: 300,
+                opacity: .55,
+                letterSpacing: .4,
+                lineHeight: 1.4,
+                textShadow: "1px 1px 5px rgba(10,10,10,.75)",
+                borderLeft: 2,
+                borderColor: "primary.main",
+                pl: 2,
+              }}
+            >
+              {item.description}
+            </Typography>
+          </Grid>
+        </Grid>
+        )}
+      </Carousel>
+    </Box>
   )
 }
 
