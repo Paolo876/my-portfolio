6ed3659cee167b87925ec3775c8317507981ef77
paoloBugarin <paolopaolobugarin@gmail.com>
2023-08-06 23:36:01 -0700

    updated contactform component

--- src/pages/Contact/ContactForm.jsx
+++ src/pages/Contact/ContactForm.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
-import { Box, Button, Stack, TextField } from '@mui/material'
+import { Box, ButtonBase, TextField } from '@mui/material'
 
 const inputStyles = {
   sx: {
     fontSize: { xs: 13, sm: 15, md:16 },
     fontWeight: 400,
     letterSpacing: .25,
-
+    textShadow: "1px 1px 5px rgba(10,10,10,.75)",
+    "&:hover": {
+      textShadow: "1px 1px 3px rgba(100,100,100,.75)",
+    }
   }
 }
 
@@ -16,6 +19,7 @@ const inputLabelStyles = {
     fontWeight: 200,
     opacity: .75,
     letterSpacing: .75,
+    textShadow: "1px 1px 5px rgba(10,10,10,.75)",
   }
 }
 
@@ -26,14 +30,34 @@ const ContactForm = () => {
     e.preventDefault();
   }
   return (
-    <Stack sx={{py: {xs: 1, sm:3,md:5}, mb: {xs: 3, sm: 0}}} align="center">
-      <Box sx={{p: {xs: 1.5, sm:2.5, md:3}, border: 3, minHeight: {md: "60vh"}, display: "flex", flexDirection: "column"}} component="form" autoComplete="off" onSubmit={handleSubmit}>
-        <Box sx={{display: "flex", flexDirection: {xs: "column", sm: "row"}, gap: {xs: 0, sm: 3}, my: {xs: 1, md: 1.5}}}>
+    <Box 
+      sx={{
+        py: {xs: 1, sm:3,md:5}, 
+        mb: {xs: 3, sm: 0}
+      }} 
+      align="center"
+    >
+      <Box 
+        sx={{
+          p: {xs: 1.5, sm:2.5, md:3,lg: 4}, 
+          border: 2, 
+          borderColor: "rgba(100,100,100,.75)",
+          // minHeight: {md: "60vh"}, 
+          display: "flex", 
+          flexDirection: "column",
+          boxShadow: 8,
+          backgroundColor: "rgba(45,45,45,.25)"
+        }} 
+        component="form" 
+        autoComplete="off" 
+        onSubmit={handleSubmit}
+      >
+        <Box sx={{display: "flex", flexDirection: {xs: "column", sm: "row"}, gap: {xs: 0, sm: 3}, my: {xs: 1, md: 1.5, lg: 2}}}>
           <TextField id="firstName" label="First Name" variant="standard" fullWidth type="text" InputProps={inputStyles} InputLabelProps={inputLabelStyles}/>
           <TextField id="lastName" label="Last Name" variant="standard" fullWidth type="text" InputProps={inputStyles} InputLabelProps={inputLabelStyles}/>
         </Box>
-        <TextField id="phone" label="Contact Number" variant="standard" fullWidth type="phone" sx={{my: {xs: 1, md: 1.5}}} InputProps={inputStyles} InputLabelProps={inputLabelStyles}/>
-        <TextField id="email" label="Email Address" variant="standard" fullWidth type="email" sx={{my: {xs: 1, md: 1.5}}} InputProps={inputStyles} InputLabelProps={inputLabelStyles}/>
+        <TextField id="phone" label="Contact Number" variant="standard" fullWidth type="phone" sx={{my: {xs: 1, md: 1.5, lg: 2}}} InputProps={inputStyles} InputLabelProps={inputLabelStyles}/>
+        <TextField id="email" label="Email Address" variant="standard" fullWidth type="email" sx={{my: {xs: 1, md: 1.5, lg: 2}}} InputProps={inputStyles} InputLabelProps={inputLabelStyles}/>
         <TextField
           id="message"
           label="Message"
@@ -44,23 +68,36 @@ const ContactForm = () => {
           variant="outlined"
           InputProps={inputStyles} InputLabelProps={inputLabelStyles}
         />
-        <Box>
-          <Button 
+        <Box my={{xs: 1, sm: 3, md: 4, }}>
+          <ButtonBase 
             type="submit" 
-            variant="contained" 
+            color="inherit"
             sx={{
-              my:{xs: .5, md: 5, lg: 8}, 
-              px: {xs:3, sm:4, md:5}, 
-              fontWeight: 500, 
-              letterSpacing: 1.75, 
-              fontSize: {xs: 11, sm: 12, md: 14, lg: 15, xl: 16}
+              boxShadow: 5,
+              border: 2, 
+              fontSize: { xs: ".9rem", sm: "1rem", md: "1rem", lg: "1.1rem" },
+              lineHeight: 1.4, 
+              borderColor: {xs: "primary.dark", sm:"rgba(100,100,100,.75)"},
+              px: {xs: 1.5, sm: 2, md: 3, lg: 4},
+              py: 1,
+              fontWeight: 500,
+              letterSpacing: 1,
+              opacity: .75,
+              transition: "all 250ms ease-in-out",
+              "&:hover":{
+                opacity:1,
+                textShadow: "1px 1px 3px rgba(100,100,100,.75)",
+                transform: "scale(1.02)",
+                borderColor: "primary.dark",
+                letterSpacing: 1.25,
+              }
             }}
           >
             Submit
-          </Button>
+          </ButtonBase>
         </Box>
       </Box>
-    </Stack>
+    </Box>
   )
 }
 
