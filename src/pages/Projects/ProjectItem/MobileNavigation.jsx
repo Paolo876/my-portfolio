90fc70180351d6fc7ba7a60d43fd37ce9df5483b
paoloBugarin <paolopaolobugarin@gmail.com>
2023-08-26 23:49:06 -0700

    updated ui

--- src/pages/Projects/ProjectItem/MobileNavigation.jsx
+++ src/pages/Projects/ProjectItem/MobileNavigation.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import { Box, IconButton, Typography } from '@mui/material';
+import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 
-const MobileNavigation = () => {
+
+const MobileNavigation = ({}) => {
   return (
-    <div>MobileNavigation</div>
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "space-between",
+        width: "100%",
+      }}
+    >
+      <Box>
+        <IconButton size="small" sx={{p:0}} disableRipple>
+          <ArrowBackIosNewIcon sx={{fontSize: {xs: 13, filter: "drop-shadow(1px 1px 3px rgba(209,145,25,.25))"}}} color="primary"/>
+        </IconButton>
+      </Box>
+      <Box>
+        
+      </Box>
+      <Box>
+        <IconButton size="small" sx={{p:0}} disableRipple>
+          <ArrowForwardIosIcon sx={{fontSize: {xs: 13, filter: "drop-shadow(1px 1px 3px rgba(209,145,25,.25))"}}} color="primary"/>
+        </IconButton>
+      </Box>
+    </Box>
   )
 }
 
