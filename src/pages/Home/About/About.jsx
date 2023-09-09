ea446f7dc61828dd8da9cf110af7bdbc39890019
paoloBugarin <paolopaolobugarin@gmail.com>
2023-09-09 00:14:26 -0700

    updated transitions

--- src/pages/Home/About/About.jsx
+++ src/pages/Home/About/About.jsx
@@ -8,12 +8,14 @@ import { keyframes } from '@mui/system';
 
 const enterAnimation = keyframes`
   0% {
-    transform: translateX(1.5em);
     opacity: 0;
   }
   100% {
-    transform: translateX(0);
     opacity: 1;
+    background: rgba(0,0,0,.25);
+    transform: scale(1.015);
+    letterSpacing: 4.5;
+    borderColor: "primary.main";
   }
 `;
 
@@ -39,7 +41,9 @@ const About = () => {
         justifyContent: "center",
         alignItems: "center",
         width: "100%",
+        overflow: "hidden"
       }}
+      ref={ref}
     >
       <Box sx={{position: "absolute", top: 0, left: 0, height: "100%", width: "100%", background: "rgba(0,0,0,.1)", zIndex: -1}}></Box>
 
@@ -58,12 +62,10 @@ const About = () => {
           justifyContent: "center",
           alignItems: "center",
           transition: "all 800ms ease-in-out",
-          "&:hover": {
-            background: "rgba(0,0,0,.25)",
-          },
+
+          background: inView ? "rgba(0,0,0,.25)" : "none",
+          transition: "all 800ms ease-in-out",
           "&:hover .content": {
-              transform: "scale(1.015)",
-              opacity: 1,
               letterSpacing: 4.5,
               borderColor: "primary.main",
             }
@@ -76,11 +78,13 @@ const About = () => {
               sx={{
                 opacity: .6,
                 transition: "all 500ms ease-in-out",
-                letterSpacing: 3,
                 textShadow: "1px 1px 7px rgba(10,10,10,.5)",
                 borderBottom: 2,
                 pb: .5,
+                letterSpacing: 3,
                 borderColor: "primary.dark",
+                transform: inView ? "scale(1.015)" : "scale(1)",
+
               }}
               className="content"
             >
