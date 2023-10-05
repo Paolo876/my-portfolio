d1e21d39bd18565179c0b0f399bc72118ea51703
paoloBugarin <paolopaolobugarin@gmail.com>
2023-10-04 23:27:02 -0700

    updated projectINtroduction

--- src/pages/Projects/ProjectItem/ProjectIntroduction.jsx
+++ src/pages/Projects/ProjectItem/ProjectIntroduction.jsx
@@ -46,56 +46,61 @@ const ProjectIntroduction = ({ title, description, logo }) => {
         </Fade> */}
       </Grid>
 
-      <Grid item xs={12} sm={7.15} md={7.5} lg={7} xl={6} sx={{}}>
-      <Fade appear={true} in={true} timeout={1100}  style={{ transitionDelay: "150ms" }}>
-          <Box>
-            <Typography 
-              variant="h2" 
-              align="left" 
-              fontWeight={400}
-              fontSize={{xs: 42, sm: 44, md: 48, lg: 55, xl: 65}} 
-              letterSpacing={.1}
-              lineHeight={{xs:1}} 
-              textTransform="none"
-              sx={{
-                textShadow: "1px 1px 7px rgba(10,10,10,.75)",
-              }}
-            >
-              {title}
-            </Typography>
-          </Box>
-        </Fade>
-          <Box sx={{position: "relative", mt: {xs: 5, sm: 7, md: 8, lg: 9, xl: 10}}}>
-            <Box 
-              sx={{
-                height: "100%", 
-                position: "absolute", 
-                left: 0, top: 0,
-                opacity: 0,
-                animation: `${slideDown} 500ms ease forwards 250ms`,
-              }}
-            ></Box>
-            <Fade appear={true} in={true} timeout={700}  style={{ transitionDelay: "800ms" }}>
+      <Grid item xs={12} sm={7.15} md={7.5} lg={7} xl={8} sx={{}}>
+        <Grid container>
+          <Grid item xs={12}>
+            <Fade appear={true} in={true} timeout={1100}  style={{ transitionDelay: "150ms" }}>
               <Box>
                 <Typography 
-                  variant="body1" 
+                  variant="h2" 
+                  align="left" 
+                  fontWeight={400}
+                  fontSize={{xs: 42, sm: 44, md: 48, lg: 55, xl: 65}} 
+                  letterSpacing={.1}
+                  lineHeight={{xs:1}} 
+                  textTransform="none"
                   sx={{
-                    fontSize: {xs: 10, sm: 11, md: 12, lg:14, xl: 14.5},
-                    fontWeight: 200,
-                    opacity: .5,
-                    letterSpacing: {xs: .75, sm: 1, md: 1.2, lg: 1.25, xl:1.3},
-                    lineHeight: 1.7,
-                    textShadow: "1px 1px 5px rgba(10,10,10,.75)",
-                    // pl: {xs: .85, sm: 1, md:1.25, lg: 1.5},
-                  }}        
-                >{description}
+                    textShadow: "1px 1px 7px rgba(10,10,10,.75)",
+                  }}
+                >
+                  {title}
                 </Typography>
               </Box>
             </Fade>
-          </Box>
-          
+          </Grid>
+          <Grid item xl={9}>
+            <Box sx={{position: "relative", mt: {xs: 5, sm: 7, md: 8, lg: 9, xl: 10}}}>
+              <Box 
+                sx={{
+                  height: "100%", 
+                  position: "absolute", 
+                  left: 0, top: 0,
+                  opacity: 0,
+                  animation: `${slideDown} 500ms ease forwards 250ms`,
+                }}
+              ></Box>
+              <Fade appear={true} in={true} timeout={700}  style={{ transitionDelay: "800ms" }}>
+                <Box>
+                  <Typography 
+                    variant="body1" 
+                    sx={{
+                      fontSize: {xs: 10, sm: 11, md: 12, lg:14, xl: 14.5},
+                      fontWeight: 200,
+                      opacity: .5,
+                      letterSpacing: {xs: .75, sm: 1, md: 1.2, lg: 1.25, xl:1.3},
+                      lineHeight: 1.7,
+                      textShadow: "1px 1px 5px rgba(10,10,10,.75)",
+                      // pl: {xs: .85, sm: 1, md:1.25, lg: 1.5},
+                    }}        
+                  >{description}
+                  </Typography>
+                </Box>
+              </Fade>
+            </Box>
+          </Grid>
+        </Grid>         
       </Grid>
-      <Grid item xs={0} sm={4.25} md={4} lg={4} xl={3} 
+      <Grid item xs={0} sm={4.25} md={4} lg={4} xl={2.5} 
         // pt={{sm: 3, md: 2, lg:1, xl: 2.5}} 
         sx={{display: {xs: "none", sm: "initial"}}}>
         <Fade appear={true} in={true} timeout={1000}  style={{ transitionDelay: "1600ms" }}>
