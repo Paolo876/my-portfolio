85d991bbbe76985b5e0bb3acdd3e24f036196aa8
paoloBugarin <paolopaolobugarin@gmail.com>
2024-02-13 23:23:49 -0800

    updated root redux

--- functions/index.js
+++ functions/index.js
@@ -15,5 +15,19 @@ exports.getImagekitKeys = functions.https.onRequest((req, res) => {
   });
 });
 
+
+exports.getEmailJSKeys = functions.https.onRequest((req, res) => {
+  cors(req, res, () => {
+    
+    res.send({
+      data: {
+        publicKey: process.env.EMAILJS_PUBLIC_KEY,
+        serviceID: process.env.EMAILJS_SERVICE_ID,
+        templateID: process.env.EMAILJS_TEMPLATE_ID,
+      },
+    });
+  });
+});
+
 // firebase deploy --only functions
 // only 6 env variable free
--- src/redux/reducers/rootReducers.js
+++ src/redux/reducers/rootReducers.js
@@ -5,13 +5,25 @@ import { httpsCallable } from 'firebase/functions';
 
 
 /** getImageKeys
- *  @desc get imagekit-io keys { urlEndpoint, publicKeys }
+ *  @desc get imagekit-io keys { urlEndpoint, publicKey }
  */
 export const getImageKeys = createAsyncThunk( 'root/getImageKeys', async ( payload, { rejectWithValue }) => {
     try {
-        // let { data } = await httpsCallable(functions, "getImagekitKeys")()
-        // const data = {}
-        return ;
+        let { data } = await httpsCallable(functions, "getImagekitKeys")()
+        return data;
+    } catch (err){
+        return rejectWithValue(err.response.data)
+    }
+})
+
+
+/** getEmailJSKeys
+ *  @desc get emailJS keys { publicKey, serviceID, templateID }
+ */
+export const getEmailJSKeys = createAsyncThunk( 'root/getEmailJSKeys', async ( payload, { rejectWithValue }) => {
+    try {
+        let { data } = await httpsCallable(functions, "getEmailJSKeys")()
+        return data;
     } catch (err){
         return rejectWithValue(err.response.data)
     }
