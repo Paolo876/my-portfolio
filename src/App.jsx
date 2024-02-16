75e25425f53a8a2798d96b61f280451c024a7867
paoloBugarin <paolopaolobugarin@gmail.com>
2024-02-16 00:10:20 -0800

    updated app functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
+import { Box } from "@mui/material";
 import "./index.scss";
 
 //components
@@ -15,6 +16,7 @@ import Home from "./pages/Home/Home";
 import Projects from "./pages/Projects/Projects";
 import About from "./pages/About/About";
 import Contact from "./pages/Contact/Contact";
+import LoadingIcon from "./components/LoadingIcon";
 
 
 function App() {
@@ -22,28 +24,58 @@ function App() {
   const { getProjects } = useProjectsRedux();
   const { getDocumentFromCollection } = useFirestoreActions();
 
+  const [ isInitLoading, setIsInitLoading ] = useState(true);
+  const [ error, setError ] = useState(false)
+  
   //run on init
   useEffect(() => {
-    getImageKeys()
-    getProjects()
-    getEmailJSKeys()
-    getDocumentFromCollection('user', 'information').then(data => setData(data))
+    initFunctions();
   }, [])
 
-  
+  const initFunctions = async () => {
+    try {
+      setIsInitLoading(true)
+      getImageKeys()
+      getProjects()
+      getEmailJSKeys()
+      const data = await getDocumentFromCollection('user', 'information')
+      setData(data);
+      setIsInitLoading(false)
+    } catch(err) {
+      setIsInitLoading(false)
+      setError(true);
+    }
+
+  }
   return (
     <div className="App">
       <div className="noise-overlay"></div>
-      <Navbar/>
-
-      {!isLoading && <div>
-        <Routes>
-          <Route element={<Home/>} path="/"/>
-          <Route element={<Projects/>} path="/projects/*"/>
-          <Route element={<About/>} path="/about"/>
-          <Route element={<Contact/>} path="/contact"/>
-        </Routes>
-      </div>}
+      {(isLoading || isInitLoading) && <Box 
+        sx={{
+          position: "absolute",
+          height: "100vh",
+          width: "100vw",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          zIndex: 500,
+          backdropFilter: "blur(15px) brightness(10%)",
+          background: "black"
+        }}
+      >
+        <LoadingIcon/>
+      </Box>}
+      {!isLoading && !isInitLoading && <>
+        <Navbar/>
+        <div>
+          <Routes>
+            <Route element={<Home/>} path="/"/>
+            <Route element={<Projects/>} path="/projects/*"/>
+            <Route element={<About/>} path="/about"/>
+            <Route element={<Contact/>} path="/contact"/>
+          </Routes>
+        </div>
+      </>}
     </div>
   );
 }
